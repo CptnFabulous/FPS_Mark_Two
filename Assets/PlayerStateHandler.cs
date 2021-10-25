a4ccd2c4b8ab0ca43901d1d86e67da740e6b2bb6
Marcus Mouyat <marcus@mouyat.com>
2021-10-25 13:06:19 +1100

    Initial commit

---
+++ Assets/PlayerStateHandler.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class PlayerStateHandler : MonoBehaviour
+{
+    public Canvas headsUpDisplay;
+    public UnityEvent onResume;
+    public Canvas pauseMenu;
+    public UnityEvent onPause;
+
+    public enum PlayerState
+    {
+        Active,
+        Paused,
+        InMenus,
+        Dead,
+    }
+
+    [SerializeField]
+    PlayerState state = PlayerState.Active;
+    public PlayerState CurrentState
+    {
+        get
+        {
+            return state;
+        }
+        set
+        {
+            state = value;
+            switch (value)
+            {
+                case PlayerState.Paused:
+
+                    Time.timeScale = 0;
+                    SwitchMenu(pauseMenu);
+                    EnterMenu();
+
+                    break;
+
+                default: // Resume game
+
+                    SwitchMenu(headsUpDisplay);
+                    ReturnToGameplay();
+
+                    break;
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentState = CurrentState; // Assigns value to itself to trigger the appropriate code
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause")) // Toggle between paused and unpaused
+        {
+            switch(CurrentState)
+            {
+                case PlayerState.Active: // pause game
+                    CurrentState = PlayerState.Paused;
+                    break;
+                case PlayerState.Paused: // resume game
+                    CurrentState = PlayerState.Active;
+                    break;
+                case PlayerState.InMenus: // exit out of menu
+                    CurrentState = PlayerState.Active;
+                    break;
+            }
+        }
+    }
+
+
+    
+    
+    void SwitchMenu(Canvas currentMenu)
+    {
+        headsUpDisplay.gameObject.SetActive(false);
+        pauseMenu.gameObject.SetActive(false);
+
+        currentMenu.gameObject.SetActive(true);
+    }
+
+    void EnterMenu()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    void ReturnToGameplay()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        Time.timeScale = 1;
+    }
+}
---
+++ Assets/RandomSoundPlayer.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Random Sound Player", menuName = "ScriptableObjects/Random Sound Player", order = 0)]
+public class RandomSoundPlayer : ScriptableObject
+{
+    public AudioClip[] sounds;
+    [Range(-3, 3)]
+    public float minPitchVariance = 1;
+    [Range(-3, 3)]
+    public float maxPitchVariance = 1;
+    [Range(0, 1)]
+    public float minVolumeVariance = 1;
+    [Range(0, 1)]
+    public float maxVolumeVariance = 1;
+    public float delay;
+
+    WaitForSeconds delayYield;
+
+    public void Play(AudioSource source)
+    {
+        if (delay <= 0)
+        {
+            PlayWithoutDelay(source);
+            return;
+        }
+        MonoBehaviour behaviourToRunFrom = source.GetComponent<MonoBehaviour>();
+        behaviourToRunFrom.StartCoroutine(DelayPlay(source));
+    }
+
+    public void PlayWithoutDelay(AudioSource source)
+    {
+        source.pitch = Random.Range(minPitchVariance, maxPitchVariance);
+        source.volume = Random.Range(minVolumeVariance, maxVolumeVariance);
+
+        int index = Random.Range(0, sounds.Length - 1);
+
+        //Debug.Log("Playing sound clip " + sounds[index].name);
+        source.PlayOneShot(sounds[index]);
+    }
+
+    public void PlayWithoutSource(Transform positionTransform)
+    {
+        int index = Random.Range(0, sounds.Length - 1);
+        float volume = Random.Range(minVolumeVariance, maxVolumeVariance);
+        AudioSource.PlayClipAtPoint(sounds[index], positionTransform.position, volume);
+    }
+
+
+    
+
+    public IEnumerator DelayPlay(AudioSource source)
+    {
+        if (delayYield == null)
+        {
+            delayYield = new WaitForSeconds(delay);
+        }
+        yield return delayYield;
+
+
+        PlayWithoutDelay(source);
+    }
+}
