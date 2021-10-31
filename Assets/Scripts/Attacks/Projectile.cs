aee8717e24c48dacfcdd49000908a2a0c845383c
Marcus Mouyat <marcus@mouyat.com>
2021-11-01 10:46:19 +1100

    Projectile system
    Created a custom projectile class with my own simple physics system. I want to be able to have bullets with travel time and drop-off due to gravity, but Unity’s physics system will glitch out when objects are moving at high speeds, often clipping through walls and failing to detect objects. My projectile system instead uses translation and raycasts, resulting in reliable detection with no glitches.
    I used a very similar system in my old project, but I made some particular differences here – firstly, the system for movement, rotation and velocity is altered to be simpler and have somewhat more realistic arcs, and secondly, code for damaging is not included, so additional functions can be added to the projectile in a modular manner. While this is a solo project, learning to make functions less code-based and more modular will make the system more accessible to artists and other developers who want to implement or update features but don’t understand code.

---
+++ Assets/Scripts/Attacks/Projectile.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Projectile : MonoBehaviour
+{
+    public Entity spawnedBy;
+    public float weight = 1;
+    public float diameter = 0.05f;
+    public float startingVelocity = 100;
+    public LayerMask detection = ~0;
+    // public UnityEvent<RaycastHit, Vector3> onHit;
+    public UnityEvent<RaycastHit> onHit;
+
+    Vector3 velocity;
+    RaycastHit surfaceHit;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocity = transform.forward * startingVelocity;
+    }
+
+    float DetectionLength
+    {
+        get
+        {
+            return velocity.magnitude * Time.deltaTime;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(transform.position, velocity, Color.red);
+        transform.LookAt(transform.position + velocity);
+        if (Physics.SphereCast(transform.position, diameter, velocity, out surfaceHit, DetectionLength, detection))
+        {
+            OnHit(surfaceHit);
+        }
+        else // Move bullet
+        {
+            transform.Translate(velocity.normalized * DetectionLength, Space.World);
+            velocity = Vector3.MoveTowards(velocity, Physics.gravity, weight * Time.deltaTime);
+        }
+        Debug.DrawRay(transform.position, velocity, Color.green);
+    }
+
+    
+
+    public void OnHit(RaycastHit thingHit)
+    {
+        //Debug.Log(thingHit.collider.name);
+        onHit.Invoke(thingHit);
+    }
+
+
+
+    public void Ricochet(float velocityDecayMultiplier = 0.75f)
+    {
+        Vector3 newDirection = Vector3.Reflect(velocity, surfaceHit.normal).normalized;
+        float outDistance = DetectionLength - surfaceHit.distance;
+        transform.position = surfaceHit.point + newDirection * outDistance;
+        velocity = newDirection * velocity.magnitude * velocityDecayMultiplier;
+        velocity = Vector3.MoveTowards(velocity, Physics.gravity, weight * Time.deltaTime);
+    }
+
+    public void CheckIfStopped(float velocityThreshold)
+    {
+        if (velocity.magnitude < velocityThreshold)
+        {
+            enabled = false;
+        }
+    }
+
+
+    public void StickObjectToSurface(RaycastHit surface, GameObject objectToStick, Vector3 rotationAxis)
+    {
+
+    }
+
+}
