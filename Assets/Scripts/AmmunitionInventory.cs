7175fe4f4de01c07c6a9e03cbe7c8bc3f33b1f38
Marcus Mouyat <marcus@mouyat.com>
2021-11-01 18:24:35 +1100

    Ammunition Inventory
    Made an AmmunitionInventory MonoBehaviour to manage quantities and capacities for the player’s ammunition reserves. This code is very similar to the version in my old project, but with some minor changes to account for the Resource struct, which was a class in my previous project.
    Firstly, there is an AmmunitionType enum used to easily determine the appropriate type, for purposes such as ammunition consumption and pickups that restore specific types. To ensure the right amount of fields are present for the amount of types, an array exists that is automatically filled in during the OnValidate function. The code will get a list of names based off the enum’s values, create a new Resource struct array, and check them against the current entries. If an entry is not present, it will create a new one. If an entry already exists and has values filled in, it will determine said info via the Resource’s name value and copy the values over to the new array. The new array will then overwrite the old one, deleting any redundant entries.
    This system is a foolproof way to update the array, adding new types and removing old ones without deleting information for existing types. For consuming and restoring ammunition, the Spend and Collect functions are used, which use the AmmunitionType enum as parameters to select the appropriate array. The only change I may make is to change the enum to a ScriptableObject, allowing me to add new parameters for each type, such as a description and HUD icon.

---
+++ Assets/Scripts/AmmunitionInventory.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AmmunitionType
+{
+    Pistol,
+    Buckshot,
+    Rifle,
+    Grenade
+}
+
+public class AmmunitionInventory : MonoBehaviour
+{
+    public Resource[] ammunitionTypes;
+    //public 
+
+    public void Collect(AmmunitionType type, int amount)
+    {
+        ammunitionTypes[(int)type].Change(amount);
+    }
+
+    public void Spend(AmmunitionType type, int amount)
+    {
+        ammunitionTypes[(int)type].Change(-amount);
+    }
+
+
+
+    void Reset()
+    {
+        OnValidate();
+    }
+    void OnValidate()
+    {
+        string[] names = System.Enum.GetNames(typeof(AmmunitionType)); // Obtains name strings for all ammunition types
+        Resource[] newAmmoTypes = new Resource[names.Length]; // Then creates an appropriately sized array of resource variables
+        for (int i = 0; i < newAmmoTypes.Length; i++) // For each required ammo type
+        {
+            // Check if a previous field still exists for the type, and update it if so
+            bool typeIsNew = true;
+            for (int o = 0; o < ammunitionTypes.Length; o++)
+            {
+                if (ammunitionTypes[i].name == names[i])
+                {
+                    newAmmoTypes[i] = ammunitionTypes[i];
+                    typeIsNew = false;
+                    break;
+                }
+            }
+
+            if (typeIsNew) // If old field could not be found, create one
+            {
+                // Create a new field for the current ammo type
+                newAmmoTypes[i] = new Resource(100, 100, 20);
+                newAmmoTypes[i].name = names[i];
+            }
+
+            // Clamps minimum amount to make sure it doesn't violate the carrying stats
+            newAmmoTypes[i].current = Mathf.Clamp(newAmmoTypes[i].current, 0, newAmmoTypes[i].max);
+        }
+        ammunitionTypes = newAmmoTypes;
+    }
+}
