7f6b9f8c79c2e818a492f594d068ca8304655502
Marcus Mouyat <marcus@mouyat.com>
2021-11-01 18:42:00 +1100

    AmmunitionType ScriptableObjects
    I quickly changed the ammunition type system to use ScriptableObjects rather than an enum. This gives me a bit more flexibility as I can assign extra values to each type, such as descriptions, HUD icons and more flexible and complicated names (since enums don’t allow spaces or certain characters). I also created four new instances to test the system.

--- Assets/Scripts/AmmunitionInventory.cs
+++ Assets/Scripts/AmmunitionInventory.cs
@@ -2,30 +2,73 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum AmmunitionType
+/*
+public enum AmmoTypeEnum
 {
     Pistol,
     Buckshot,
     Rifle,
     Grenade
 }
-
+*/
 public class AmmunitionInventory : MonoBehaviour
 {
     public Resource[] ammunitionTypes;
     //public 
-
     public void Collect(AmmunitionType type, int amount)
     {
-        ammunitionTypes[(int)type].Change(amount);
+        ammunitionTypes[AmmunitionType.GetIndex(type)].Change(amount);
     }
 
     public void Spend(AmmunitionType type, int amount)
     {
-        ammunitionTypes[(int)type].Change(-amount);
+        ammunitionTypes[AmmunitionType.GetIndex(type)].Change(-amount);
     }
 
+    void Reset()
+    {
+        OnValidate();
+    }
+    void OnValidate()
+    {
+        Resource[] newAmmoTypes = new Resource[AmmunitionType.All.Length]; // Then creates an appropriately sized array of resource variables
+        for (int i = 0; i < newAmmoTypes.Length; i++) // For each required ammo type
+        {
+            // Check if a previous field still exists for the type, and update it if so
+            bool typeIsNew = true;
+            for (int o = 0; o < ammunitionTypes.Length; o++)
+            {
+                if (ammunitionTypes[i].name == AmmunitionType.All[i].name)
+                {
+                    newAmmoTypes[i] = ammunitionTypes[i];
+                    typeIsNew = false;
+                    break;
+                }
+            }
 
+            if (typeIsNew) // If old field could not be found, create one
+            {
+                // Create a new field for the current ammo type
+                newAmmoTypes[i] = new Resource(100, 100, 20);
+                newAmmoTypes[i].name = AmmunitionType.All[i].name;
+            }
+
+            // Clamps minimum amount to make sure it doesn't violate the carrying stats
+            newAmmoTypes[i].current = Mathf.Clamp(newAmmoTypes[i].current, 0, newAmmoTypes[i].max);
+        }
+        ammunitionTypes = newAmmoTypes;
+    }
+
+    /*
+    public void Collect(AmmoTypeEnum type, int amount)
+    {
+        ammunitionTypes[(int)type].Change(amount);
+    }
+
+    public void Spend(AmmoTypeEnum type, int amount)
+    {
+        ammunitionTypes[(int)type].Change(-amount);
+    }
 
     void Reset()
     {
@@ -61,4 +104,5 @@ public class AmmunitionInventory : MonoBehaviour
         }
         ammunitionTypes = newAmmoTypes;
     }
+    */
 }
---
+++ Assets/Scripts/AmmunitionType.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Ammunition Type", menuName = "ScriptableObjects/Ammunition Type", order = 0)]
+public class AmmunitionType : ScriptableObject
+{
+    public string description;
+    public Sprite icon;
+
+
+
+
+
+
+    public static AmmunitionType[] All
+    {
+        get
+        {
+            if (allTypes == null)
+            {
+                allTypes = Resources.LoadAll<AmmunitionType>("");
+            }
+            return allTypes;
+        }
+    }
+    static AmmunitionType[] allTypes;
+
+    public static int GetIndex(AmmunitionType type)
+    {
+        for (int i = 0; i < All.Length; i++)
+        {
+            if (All[i] == type)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
