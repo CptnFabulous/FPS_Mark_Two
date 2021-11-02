25669b960d1e66bf368ac06a005b89863ee3e7f9
Marcus Mouyat <marcus@mouyat.com>
2021-11-02 18:49:04 +1100

    Added a check to ensure damage cannot be dealt to something that is already dead, unless a value is checked specifying otherwise

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -10,6 +10,14 @@ public class Health : MonoBehaviour
     public UnityEvent<int> onDamage;
     public UnityEvent<int> onHeal;
     public UnityEvent<int> onDeath;
+    public bool allowPosthumousDamage;
+    public bool IsAlive
+    {
+        get
+        {
+            return data.current > 0;
+        }
+    }
 
     private void Awake()
     {
@@ -18,10 +26,16 @@ public class Health : MonoBehaviour
         {
             hitboxes[i].sourceHealth = this;
         }
+        onHeal.Invoke(0);
     }
 
     public void Damage(int amount, Entity attacker)
     {
+        if (IsAlive == false && allowPosthumousDamage == false)
+        {
+            return;
+        }
+        
         data.current -= amount;
         if (data.current <= 0)
         {
