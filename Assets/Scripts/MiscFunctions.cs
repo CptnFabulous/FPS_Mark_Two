25c91ec4d3105ad44614fbaefc74803a220a6cfc
Marcus Mouyat <marcus@mouyat.com>
2021-11-05 15:26:19 +1100

    Removed miscellaneous functions for producing angled directions
    The code was poorly implemented and confusing, and I could implement the same effect more convincingly by creating Quaternions and multiplying them by directions.

--- Assets/Scripts/MiscFunctions.cs
+++ Assets/Scripts/MiscFunctions.cs
@@ -39,24 +39,6 @@ public readonly struct MiscFunctions
     }
 
 
-    public static Vector3 PerpendicularRight(Vector3 forward, Vector3 worldUp)
-    {
-        return Vector3.Cross(forward, worldUp).normalized;
-    }
-
-    public static Vector3 PerpendicularUp(Vector3 forward, Vector3 worldUp)
-    {
-        Vector3 worldRight = PerpendicularRight(forward, worldUp);
-        return Vector3.Cross(forward, worldRight).normalized;
-    }
-
-    public static Vector3 AngledDirection(Vector3 axes, Vector3 forward, Vector3 upward)
-    {
-        Vector3 right = PerpendicularRight(forward, upward);
-        Vector3 up = PerpendicularUp(forward, upward);
-        return Quaternion.AngleAxis(axes.x, right) * Quaternion.AngleAxis(axes.y, up) * Quaternion.AngleAxis(axes.z, forward) * forward;
-    }
-
 
     /// <summary>
     /// Checks if a number key is pressed and produces an array index. If startWithOne is true, values are shifted down to represent number positions on a num row, otherwise the int accurately represents the number pressed.
