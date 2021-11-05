fdbeb0451ec767f884a5daa10056a294587bbbfb
Marcus Mouyat <marcus@mouyat.com>
2021-11-05 15:28:45 +1100

    Fixed a bug preventing projectiles from aiming properly if the raycast check does not land
    The position for specifying where the bullet should be aimed if the raycast does not land (projectileDirection) was simply a direction value. Since the origin of the shot was not added, the game interpreted it as the direction of the shot from 0,0,0 regardless of the player's position. I fixed this by adding the player's aim origin to the value.
    
    I also added several comments for improved clarity.

--- Assets/Scripts/Player Weapons/GunGeneralStats.cs
+++ Assets/Scripts/Player Weapons/GunGeneralStats.cs
@@ -21,20 +21,24 @@ public class GunGeneralStats : MonoBehaviour
 
         for (int i = 0; i < projectileCount; i++)
         {
+            // Instantiate a projectile, enable its gameobject and specify its user
             Projectile newProjectile = Instantiate(projectilePrefab);
             newProjectile.spawnedBy = user;
             newProjectile.gameObject.SetActive(true);
 
-            Vector3 spreadAngles = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
-            spreadAngles = spreadAngles.normalized * shotSpread;
-            Vector3 individualShotDirection = MiscFunctions.AngledDirection(spreadAngles, aimDirection, worldUp);
-            Vector3 hitPoint = individualShotDirection.normalized * range;
-            if (Physics.Raycast(origin, individualShotDirection, out RaycastHit surfaceHit, range, projectilePrefab.detection))
+            // Calculates a direction for the projectile, given random spread angles
+            Vector3 spreadAngles = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)) * shotSpread;
+            Vector3 projectileDirection = Quaternion.LookRotation(aimDirection, worldUp) * Quaternion.Euler(spreadAngles) * Vector3.forward;
+            // Calculates a point out based on the angle and range, to aim the projectile if the subsequent raycast fails
+            Vector3 hitPoint = origin + (projectileDirection * range);
+            if (Physics.Raycast(origin, projectileDirection, out RaycastHit surfaceHit, range, projectilePrefab.detection))
             {
+                // Launches a raycast to the same point to check if there is a hit point, and updates if so
                 hitPoint = surfaceHit.point;
             }
             
-            if (Vector3.Angle(hitPoint - muzzle.position, individualShotDirection) < 90) // If hit point is in front of muzzle (like normal)
+            // Angles projectile towards the appropriate hit point
+            if (Vector3.Angle(hitPoint - muzzle.position, projectileDirection) < 90) // If hit point is in front of muzzle (like normal)
             {
                 newProjectile.transform.position = muzzle.position;
                 newProjectile.transform.LookAt(hitPoint, worldUp);
@@ -42,7 +46,8 @@ public class GunGeneralStats : MonoBehaviour
             else // If for whatever reason the hit point is behind the muzzle
             {
                 newProjectile.transform.position = hitPoint;
-                newProjectile.transform.LookAt(transform.position + individualShotDirection, worldUp);
+                newProjectile.transform.LookAt(transform.position + projectileDirection, worldUp);
+                // Projectile does not need to move, activate OnHit immediately
                 newProjectile.OnHit(surfaceHit);
             }
         }
