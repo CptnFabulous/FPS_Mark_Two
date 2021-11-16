eaae1dbd06c684a02d72c37b9b5c656008b617de
Marcus Mouyat <marcus@mouyat.com>
2021-11-16 11:57:02 +1100

    Removed old enum-based code from AmmunitionInventory
    Now that the system works reliably using ScriptableObjects for ammunition types (for improved names and extra information), there is no need for that code.

--- Assets/Scripts/Player/AmmunitionInventory.cs
+++ Assets/Scripts/Player/AmmunitionInventory.cs
@@ -14,8 +14,6 @@ public enum AmmoTypeEnum
 public class AmmunitionInventory : MonoBehaviour
 {
     public Resource[] ammunitionTypes;
-    //public 
-    
     
     public Resource GetValues(AmmunitionType type)
     {
@@ -71,51 +69,4 @@ public class AmmunitionInventory : MonoBehaviour
         }
         ammunitionTypes = newAmmoTypes;
     }
-
-    /*
-    public void Collect(AmmoTypeEnum type, int amount)
-    {
-        ammunitionTypes[(int)type].Change(amount);
-    }
-
-    public void Spend(AmmoTypeEnum type, int amount)
-    {
-        ammunitionTypes[(int)type].Change(-amount);
-    }
-
-    void Reset()
-    {
-        OnValidate();
-    }
-    void OnValidate()
-    {
-        string[] names = System.Enum.GetNames(typeof(AmmunitionType)); // Obtains name strings for all ammunition types
-        Resource[] newAmmoTypes = new Resource[names.Length]; // Then creates an appropriately sized array of resource variables
-        for (int i = 0; i < newAmmoTypes.Length; i++) // For each required ammo type
-        {
-            // Check if a previous field still exists for the type, and update it if so
-            bool typeIsNew = true;
-            for (int o = 0; o < ammunitionTypes.Length; o++)
-            {
-                if (ammunitionTypes[i].name == names[i])
-                {
-                    newAmmoTypes[i] = ammunitionTypes[i];
-                    typeIsNew = false;
-                    break;
-                }
-            }
-
-            if (typeIsNew) // If old field could not be found, create one
-            {
-                // Create a new field for the current ammo type
-                newAmmoTypes[i] = new Resource(100, 100, 20);
-                newAmmoTypes[i].name = names[i];
-            }
-
-            // Clamps minimum amount to make sure it doesn't violate the carrying stats
-            newAmmoTypes[i].current = Mathf.Clamp(newAmmoTypes[i].current, 0, newAmmoTypes[i].max);
-        }
-        ammunitionTypes = newAmmoTypes;
-    }
-    */
 }
