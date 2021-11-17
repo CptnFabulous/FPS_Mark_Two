1dd2eb5bd77303caf9f380f6dc52bab166631f18
Marcus Mouyat <marcus@mouyat.com>
2021-11-17 12:47:55 +1100

    Added switching functionality to menus
    During Start(), each MenuWindow code will find its parents and children in the hierarchy. If a MenuWindow does not have a parent, it is the root, and will automatically assign itself as such to itself and all other menus in the hierarchy.
    When I want to implement functionality to switch to a different menu window, I can add a SwitchWindow() function as a listener, and assign a MenuWindow. The code will then cleanly disable all windows except for the desired window and its parents, then alter the CanvasGroup alpha of each parent to hide them from being viewed and interacted with. I had to use the CanvasGroup for this because simply disabling a canvas or its gameobject will disable all child canvas objects, meaning if the current window has a parent I cannot disable it without disabling the window I actually want to look at.
    I also implemented two extra functions – ReturnToParentMenu() and ReturnToRoot(), which will automatically switch to the parent or root menu respectively without any parameters to specify. A reference for a ‘back’ button is present in the MenuWindow script, which has ReturnToParentMenu() assigned as a listener in Awake(). These functions are useful because it allows me to drop a menu prefab into a hierarchy, and it will automatically have functionality to return to its parent without me needing to assign listeners to menu buttons.

--- Assets/Scripts/Menus/MenuWindow.cs
+++ Assets/Scripts/Menus/MenuWindow.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(Canvas), typeof(CanvasGroup))]
 public class MenuWindow : MonoBehaviour
 {
     public string description = "A menu.";
@@ -14,12 +15,16 @@ public class MenuWindow : MonoBehaviour
     public Button back;
     public Image selectedGraphic;
     public Text selectionDescription;
-    
+
     Canvas canvas;
+    CanvasGroup visualElements;
+    RectTransform rt;
     EventSystem eventSystem;
 
+    
+    MenuWindow[] parents;
+    MenuWindow immediateParent;
     MenuWindow root;
-    MenuWindow parent;
     MenuWindow[] children;
 
     private void OnValidate()
@@ -32,19 +37,86 @@ public class MenuWindow : MonoBehaviour
     /*
     private void Awake()
     {
-        StandaloneInputModule s;
-        s.se
+        canvas = GetComponent<Canvas>();
+        visualElements = GetComponent<CanvasGroup>();
+        visualElements.ignoreParentGroups = true;
+        rt = GetComponent<RectTransform>();
+        eventSystem = GetComponent<EventSystem>();
+        Debug.Log(back + ", " + name);
+
+        if (back != null)
+        {
+            back.onClick.AddListener(ReturnToParentMenu);
+        }
     }
-    */
+
+    private void Start()
+    {
+        // Assign children
+        List<MenuWindow> childMenus = new List<MenuWindow>(GetComponentsInChildren<MenuWindow>(true));
+        childMenus.Remove(this);
+        children = childMenus.ToArray();
 
 
+        // Assign parents
+        List<MenuWindow> parentMenus = new List<MenuWindow>(GetComponentsInParent<MenuWindow>(true));
+        parentMenus.Remove(this);
+        parents = parentMenus.ToArray();
+        immediateParent = null;
+        if (transform.parent != null)
+        {
+            immediateParent = transform.parent.GetComponentInParent<MenuWindow>();
+        }
+
+        // If no parent could be found, this object is the root
+        if (immediateParent == null)
+        {
+            root = this;
+            for(int i = 0; i < children.Length; i++)
+            {
+                children[i].root = this;
+            }
+
+            if (back != null)
+            {
+                back.gameObject.SetActive(false); // Disable the back button if there's no parent to return to
+            }
+            
+        }
+
+        ReturnToRoot();
+    }
 
 
     public void SwitchWindow(MenuWindow newWindow)
     {
+        Debug.Log(newWindow + ", " + name);
+        // Disable all windows except for the current one and its parents
+        // The root is not part of this specific for loop but that doesn't matter, it needs to be active in order for itself or any of its children to be active
+        Debug.Log(root.children);
+        for (int i = 0; i < root.children.Length; i++)
+        {
+            root.children[i].gameObject.SetActive(false);
+        }
 
+        // Enable new window and parents, using canvas group to hide parents
+        Debug.Log(newWindow.parents);
+        newWindow.gameObject.SetActive(true);
+        newWindow.visualElements.alpha = 1;
+        for (int i = 0; i < newWindow.parents.Length; i++)
+        {
+            newWindow.parents[i].gameObject.SetActive(true);
+            newWindow.parents[i].visualElements.alpha = 0;
+        }
     }
-
+    public void ReturnToParentMenu()
+    {
+        SwitchWindow(immediateParent);
+    }
+    public void ReturnToRoot()
+    {
+        SwitchWindow(root);
+    }
 
     void RefreshSelectionInfo()
     {
