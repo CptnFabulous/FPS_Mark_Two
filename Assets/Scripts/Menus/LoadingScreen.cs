e5ef8fce09ad99eca0527829d98a39d8e244d81c
Marcus Mouyat <marcus@mouyat.com>
2021-11-27 20:10:52 +1100

    Experimented with loading screen functionality, that didn't work.
    I tried making a loading screen function that asynchronously loads a new scene in the background, while loading another scene over the top to act as a loading screen. This system did not work properly, and I opted to spend my time on other features instead.

---
+++ Assets/Scripts/Menus/LoadingScreen.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class LoadingScreen : MonoBehaviour
+{
+    public Canvas loadingScreenInfo;
+    public Text sceneTitle;
+    public UnityEvent<float> progressEffects;
+    public Canvas continueScreen;
+    public string continueInput;
+
+
+
+    public static readonly string loadingSceneName = "Loading Screen";
+
+
+    public static IEnumerator LoadSequence(string newScene)
+    {
+        AsyncOperation screenTransition = SceneManager.LoadSceneAsync(loadingSceneName, LoadSceneMode.Single);
+        // yield return new WaitUntil(() => loadScreenLoad.isDone);
+        while (screenTransition.progress < 0.9f)
+        {
+            Debug.Log("Transition to loading screen, " + screenTransition.progress);
+            yield return null;
+        }
+
+        Debug.Log("Loading scene fully loaded");
+
+        AsyncOperation newSceneLoad = SceneManager.LoadSceneAsync(newScene, LoadSceneMode.Additive);
+        newSceneLoad.allowSceneActivation = false;
+        // yield return new WaitUntil(() => sequence.isDone);
+        while (newSceneLoad.progress < 0.9f)
+        {
+            Debug.Log("Loading new scene, " + newSceneLoad.progress);
+            //screen.progressEffects.Invoke(newSceneLoad.progress);
+            yield return null;
+        }
+
+        yield return new WaitUntil(() => Input.GetButtonDown("Jump"));
+        // Unload load screen scene and activate new scene
+        SceneManager.UnloadSceneAsync(loadingSceneName);
+        newSceneLoad.allowSceneActivation = true;
+    }
+
+
+    /*
+    public static IEnumerator LoadSequence(string newScene)
+    {
+        // Unloads current scene and loads the loading screen
+        AsyncOperation loadScreenLoad = SceneManager.LoadSceneAsync(loadingSceneName, LoadSceneMode.Single);
+        Debug.Log("Pot roast");
+        yield return new WaitUntil(() => loadScreenLoad.isDone);
+        Debug.Log("Blah");
+        // Use FindObjectOfType to get a load scene management script in the loading screen scene and run load scene stuff from it
+        LoadingScreen screen = FindObjectOfType<LoadingScreen>();
+        screen.loadingScreenInfo.gameObject.SetActive(false);
+        screen.continueScreen.gameObject.SetActive(true);
+        screen.sceneTitle.text = newScene;
+        Debug.Log("Screen loaded");
+        // Starts loading of new scene asynchronously, setting a bool so it doesn't activate immediately
+        AsyncOperation sequence = SceneManager.LoadSceneAsync(newScene, LoadSceneMode.Additive);
+        sequence.allowSceneActivation = false;
+        // Initiate a loop within the IEnumerator while waiting for the sequence to load
+        while (sequence.isDone == false)
+        {
+            Debug.Log("Loop");
+            // In this loop, do things while waiting for the load sequence to complete
+            screen.progressEffects.Invoke(sequence.progress);
+            yield return null;
+        }
+
+        Debug.Log("Complete");
+        // Change load screen graphics to show new scene is ready to activate
+        screen.loadingScreenInfo.gameObject.SetActive(false);
+        screen.continueScreen.gameObject.SetActive(true);
+
+        Debug.Log("Waiting on input");
+        // Wait until the player presses the button to continue
+        yield return new WaitUntil(() => Input.GetButtonDown(screen.continueInput));
+        // Unload load screen scene and activate new scene
+        SceneManager.UnloadSceneAsync(loadingSceneName);
+        sequence.allowSceneActivation = true;
+    }
+    */
+}
