2808cae1e56c1b7c4039724a88f487b51b8aef11
Marcus Mouyat <marcus@mouyat.com>
2021-12-08 12:09:16 +1100

    Tweaked NumKeyPressed input to also include numpad keys rather than just alpha keys
    After figuring out that the keys on a keyboard's numpad were treated as separate inputs to the ones on the top row of the keyboard, I adjusted this function to account for both inputs.

--- Assets/Scripts/MiscFunctions.cs
+++ Assets/Scripts/MiscFunctions.cs
@@ -75,9 +75,25 @@ public readonly struct MiscFunctions
     /// <returns></returns>
     public static bool NumKeyPressed(out int key, bool startWithOne)
     {
-        for (int i = 0; i < numberKeys.Length; i++)
+        // Check alpha keys
+        for (int i = 0; i < alphaKeys.Length; i++)
         {
-            if (Input.GetKeyDown(numberKeys[i]))
+            if (Input.GetKeyDown(alphaKeys[i]))
+            {
+                key = i;
+                if (startWithOne)
+                {
+                    key -= 1;
+                    key = InverseClamp(key, 0, 9);
+                }
+                return true;
+            }
+        }
+
+        // Check numpad keys
+        for (int i = 0; i < numpadKeys.Length; i++)
+        {
+            if (Input.GetKeyDown(numpadKeys[i]))
             {
                 key = i;
                 if (startWithOne)
@@ -92,7 +108,7 @@ public readonly struct MiscFunctions
         key = -1;
         return false;
     }
-    public static readonly KeyCode[] numberKeys = new KeyCode[10]
+    public static readonly KeyCode[] alphaKeys = new KeyCode[10]
     {
         KeyCode.Alpha0,
         KeyCode.Alpha1,
@@ -104,6 +120,19 @@ public readonly struct MiscFunctions
         KeyCode.Alpha7,
         KeyCode.Alpha8,
         KeyCode.Alpha9,
+    };
+    public static readonly KeyCode[] numpadKeys = new KeyCode[10]
+    {
+        KeyCode.Keypad0,
+        KeyCode.Keypad1,
+        KeyCode.Keypad2,
+        KeyCode.Keypad3,
+        KeyCode.Keypad4,
+        KeyCode.Keypad5,
+        KeyCode.Keypad6,
+        KeyCode.Keypad7,
+        KeyCode.Keypad8,
+        KeyCode.Keypad9,
     };
 
 }
