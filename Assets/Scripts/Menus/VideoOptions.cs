d9b18ab1181cc33dc5b19b006245af53a5485510
Marcus Mouyat <marcus@mouyat.com>
2021-12-13 17:20:17 +1100

    Created VideoOptions submenu class for graphics settings
    This script contains code for updating the player’s fullscreen mode, resolution, refresh rate and graphical quality preset. This menu was much more difficult to implement than the GameplayOptions menu, due to the increased complexity of populating a dropdown menu, the difficulty of converting Unity’s graphical settings into usable values in GUI options, and converting those options back into usable settings when the player confirms their changes.
    The most difficult part was setting up the dropdowns for resolution and refresh rate. The big problem is that Unity does not have separate values for resolution and refresh rate. These settings are stored in a single struct value that specifies the width, the height and a refresh rate. Screen.resolutions, the array for obtaining these values, is a single-length array containing all of these values, meaning if you want to change one value you have to scroll through multiple duplicates for each resolution, to account for the many different refresh rates.
    The code I made to remedy this involves creating a list of lists (named resolutionsAndRefreshRates, to sort each set of Resolution structs for a particular width and height into their own list). I then converted Screen.resolutions into another list (named allResolutions), and used a while loop to obtain the first value in the list, make a new list (named newResolution) out of each other value with the same width and height, copy them into resolutionsAndRefreshRates and delete them from allResolutions. This keeps looping until every different type of resolution has been cut out and placed in their own list.
    I also had to implement an extra bit of code, so that every time the player changes their resolution, the refresh rate dropdown is adjusted to show the available refresh rates for that particular resolution.
    I am pleased with this code, but unfortunately tests have shown a strange issue causing the editor to freeze up. I will need to investigate this further.

---
+++ Assets/Scripts/Menus/VideoOptions.cs
@@ -0,0 +1,128 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VideoOptions : OptionsMenu
+{
+    [Header("Simple options")]
+    public Dropdown fullscreenMode;
+
+    public Dropdown resolutions;
+    public Dropdown refreshRates;
+    List<List<Resolution>> resolutionsAndRefreshRates;
+
+    public Dropdown graphicsQualityPreset;
+    public bool applyExpensiveQualityPresetChanges = true;
+
+
+    public override void ApplySettings()
+    {
+        QualitySettings.SetQualityLevel(graphicsQualityPreset.value, applyExpensiveQualityPresetChanges);
+        Resolution newResolution = resolutionsAndRefreshRates[resolutions.value][refreshRates.value];
+        Screen.SetResolution(newResolution.width, newResolution.height, (FullScreenMode)fullscreenMode.value, newResolution.refreshRate);
+    }
+
+    public override void ObtainCurrentValues()
+    {
+        #region Fullscreen
+        string[] fullScreenOptions = System.Enum.GetNames(typeof(FullScreenMode));
+        fullscreenMode.ClearOptions();
+        fullscreenMode.AddOptions(new List<string>(fullScreenOptions));
+        fullscreenMode.value = (int)Screen.fullScreenMode;
+        fullscreenMode.RefreshShownValue();
+        #endregion
+
+        #region Simple graphics quality
+        graphicsQualityPreset.ClearOptions();
+        graphicsQualityPreset.AddOptions(new List<string>(QualitySettings.names));
+        graphicsQualityPreset.value = QualitySettings.GetQualityLevel();
+        graphicsQualityPreset.RefreshShownValue();
+        #endregion
+
+        #region Sort resolutions and refresh rates
+        // Create a list of lists, so each distinct resolution has its variants put in its own list
+        resolutionsAndRefreshRates = new List<List<Resolution>>();
+        // Obtain all available Resolution structs, converted to a list of varying length
+        List<Resolution> allResolutions = new List<Resolution>(Screen.resolutions);
+        while (allResolutions.Count > 0)
+        {
+            // Check the first struct in allResolutions and find every one in the list with the same width and height
+            List<Resolution> newResolution = allResolutions.FindAll((r) => r.width == allResolutions[0].width && r.height == allResolutions[0].height);
+            // Sort list by refresh rates
+            newResolution.Sort((a, b) => a.refreshRate.CompareTo(b.refreshRate));
+            // Add this list to the tables
+            resolutionsAndRefreshRates.Add(newResolution);
+            // Remove all entries from allResolutions that were copied to newResolution, using the same check as before.
+            newResolution.RemoveAll((r) => r.width == newResolution[0].width && r.height == newResolution[0].height);
+            // This code will loop, check the next 'first' struct, copy all with an identical width and height to a separate list, and delete them from allResolutions.
+            // This will keep occurring until allResolutions is empty, and therefore that all entries have been sorted into distinct tables.
+        }
+        // Sort tables by the resolution in each one
+        resolutionsAndRefreshRates.Sort((a, b) => (a[0].width * a[0].height).CompareTo(b[0].width * b[0].height));
+        #endregion
+
+        #region Find current resolution and refresh rate, update both dropdowns and assign the appropriate values
+        FindCorrectResolutionAndRefreshRate(out int resolutionIndex, out int refreshRateIndex);
+
+        resolutions.ClearOptions();
+        for (int i = 0; i < resolutionsAndRefreshRates.Count; i++)
+        {
+            Resolution r = resolutionsAndRefreshRates[i][0];
+            Dropdown.OptionData option = new Dropdown.OptionData(r.width + " X " + r.height);
+            resolutions.options.Add(option);
+        }
+
+        UpdateRefreshRateDropdown(resolutionIndex);
+
+        resolutions.SetValueWithoutNotify(resolutionIndex);
+        refreshRates.SetValueWithoutNotify(refreshRateIndex);
+        #endregion
+    }
+    void FindCorrectResolutionAndRefreshRate(out int resolutionIndex, out int refreshRateIndex)
+    {
+        resolutionIndex = -1;
+        refreshRateIndex = -1;
+        Resolution current = Screen.currentResolution;
+        for (int i = 0; i <  resolutionsAndRefreshRates.Count; i++)
+        {
+            List<Resolution> appropriate = resolutionsAndRefreshRates[i];
+            Resolution r = appropriate[0];
+            if (current.width == r.width && current.height == r.height)
+            {
+                resolutionIndex = i;
+                for (int f = 0; f < appropriate.Count; f++)
+                {
+                    if (current.refreshRate == appropriate[f].refreshRate)
+                    {
+                        refreshRateIndex = f;
+                        return;
+                    }
+                }
+                break;
+            }
+        }
+    }
+    void UpdateRefreshRateDropdown(int resolutionIndex)
+    {
+        refreshRates.ClearOptions();
+        List<Resolution> correct = resolutionsAndRefreshRates[resolutionIndex];
+        for (int i = 0; i < correct.Count; i++)
+        {
+            Resolution r = correct[i];
+            Dropdown.OptionData option = new Dropdown.OptionData(r.refreshRate + "Hz");
+            refreshRates.options.Add(option);
+        }
+    }
+
+
+
+    public override void SetupOptions()
+    {
+        AddValueChangedEvent(resolutions);
+        resolutions.onValueChanged.AddListener(UpdateRefreshRateDropdown);
+        AddValueChangedEvent(refreshRates);
+        AddValueChangedEvent(fullscreenMode);
+        AddValueChangedEvent(graphicsQualityPreset);
+    }
+}
