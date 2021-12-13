695760118e7c36eb48aba08cc89e6daa23b365e9
Marcus Mouyat <marcus@mouyat.com>
2021-12-13 15:28:15 +1100

    Created code gameplay options menu variant
    I created a new class that inherits from OptionsMenu, named GameplayOptions. This variant contains functionality for various player control settings that don’t involve button bindings, specifically:
    •	Camera sensitivity
    •	Inverting camera axes
    •	Field of view
    •	Toggling crouching
    •	Toggling ADS

---
+++ Assets/Scripts/Menus/GameplayOptions.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameplayOptions : OptionsMenu
+{
+    [Header("Options")]
+    public Slider aimSensitivityX;
+    public Slider aimSensitivityY;
+    public float maxAimSensitivity = 500;
+    public Toggle invertX;
+    public Toggle invertY;
+    public Slider fieldOfView;
+    public Toggle toggleCrouch;
+    public Toggle toggleADS;
+
+    Player playerUpdating;
+    
+    public override void ApplySettings()
+    {
+        MovementController movement = playerUpdating.movement;
+        movement.aimSensitivity.x = SliderValueToSensitivity(aimSensitivityX, maxAimSensitivity);
+        movement.aimSensitivity.y = SliderValueToSensitivity(aimSensitivityY, maxAimSensitivity);
+        movement.invertX = invertX.isOn;
+        movement.invertY = invertY.isOn;
+        movement.fieldOfView = fieldOfView.value;
+        movement.toggleCrouch = toggleCrouch.isOn;
+
+        playerUpdating.weapons.toggleADS = toggleADS.isOn;
+    }
+
+    public override void ObtainCurrentValues()
+    {
+        playerUpdating = GetComponentInParent<Player>();
+        MovementController movement = playerUpdating.movement;
+
+        OptionsMenu.SensitivityToSliderValue(aimSensitivityX, movement.aimSensitivity.x, maxAimSensitivity);
+        OptionsMenu.SensitivityToSliderValue(aimSensitivityY, movement.aimSensitivity.y, maxAimSensitivity);
+        invertX.isOn = movement.invertX;
+        invertY.isOn = movement.invertY;
+        fieldOfView.value = movement.fieldOfView;
+        toggleCrouch.isOn = movement.toggleCrouch;
+
+        toggleADS.isOn = playerUpdating.weapons.toggleADS;
+    }
+
+    public override void SetupOptions()
+    {
+        AddValueChangedEvent(aimSensitivityX);
+        AddValueChangedEvent(aimSensitivityY);
+        AddValueChangedEvent(invertX);
+        AddValueChangedEvent(invertY);
+        AddValueChangedEvent(fieldOfView);
+        AddValueChangedEvent(toggleCrouch);
+        AddValueChangedEvent(toggleADS);
+    }
+}
