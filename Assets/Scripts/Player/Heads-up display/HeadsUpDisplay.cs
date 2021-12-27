519a270e5cb24d54cbb348da1c423a572d64b9ac
Marcus Mouyat <marcus@mouyat.com>
2021-12-27 12:37:57 +1100

    Weapon reticle in HeadsUpDisplay will now auto-hide if the weapon selector is open.

--- Assets/Scripts/Player/Heads-up display/HeadsUpDisplay.cs
+++ Assets/Scripts/Player/Heads-up display/HeadsUpDisplay.cs
@@ -183,7 +183,9 @@ public class HeadsUpDisplay : MonoBehaviour
         // Disable reticle if player is aiming down weapon sights
         // Show reticle If ADS is null, OR if ADS is present but player is not aiming/transitioning and hideDefaultReticle is false
         GunADS ads = currentMode.optics;
-        reticle.SetActive(ads == null || (!(ads.IsAiming || ads.IsTransitioning) && !ads.hideMainReticle));
+        bool notHiddenDueToAds = ads == null || (!(ads.IsAiming || ads.IsTransitioning) && !ads.hideMainReticle);
+        bool notInWeaponSelector = controller.weapons.weaponSelector.active == false;
+        reticle.SetActive(notHiddenDueToAds && notInWeaponSelector);
     }
 
 
