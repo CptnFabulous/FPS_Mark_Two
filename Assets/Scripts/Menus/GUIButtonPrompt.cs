0f1c4c421da8ae8cf43359a4cbfbedac23028a5a
Marcus Mouyat <marcus@mouyat.com>
2021-12-30 20:33:47 +1100

    Updated GUIButtonPrompt to work with new input system (untested)
    Added fields for an InputAction named ‘assignedInput’ and a PlayerInput named ‘player’. These are assigned using the AssignAction() function.
    If the prompt object is active and an InputAction and PlayerInput are assigned, an Update() function will run, that checks the current input device in PlayerInput to display the binding for said device.
    Added a series of Sprite fields to reference different keyboard, mouse and controller buttons
    Added a Sprite array property that easily collates these sprites (except for the keyboard sprite since it acts as a ‘default’ if a dedicated sprite isn’t available)
    Added a readonly string list for corresponding input mapping paths
    An individual binding is displayed using a Refresh() function. This obtains the binding’s path and checks it against the ones in the readonly list. If one is found, it uses the corresponding index to display the correct button sprite. If not, the default keyboard key sprite is displayed, and the corresponding text object will read out the button name, formatted to remove the rest of the filepath. Currently, it is not formatted to add capital letters or line breaks, but this shouldn’t be too difficult to add in future.

--- Assets/Scripts/Menus/GUIButtonPrompt.cs
+++ Assets/Scripts/Menus/GUIButtonPrompt.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class GUIButtonPrompt : MonoBehaviour
 {
@@ -9,29 +10,163 @@ public class GUIButtonPrompt : MonoBehaviour
     public Text keyName;
     Vector2Int singleKeyDimensions;
 
-    [Header("Sprites")]
-    public Sprite keyBackground;
-    public Sprite[] controllerSprites;
+    [Header("Sprites - Keyboard + mouse")]
+    public Sprite keyboardKey;
+    public Sprite mouseLeft;
+    public Sprite mouseRight;
+    public Sprite mouseMiddle;
+    public Sprite mouseMove;
+    public Sprite mouseScrollWheel;
+    [Header("Sprites - Gamepad")]
+    public Sprite leftStickMove;
+    public Sprite rightStickMove;
+    public Sprite leftStickClick;
+    public Sprite rightStickClick;
+    public Sprite faceButtonNorth;
+    public Sprite faceButtonSouth;
+    public Sprite faceButtonEast;
+    public Sprite faceButtonWest;
+    public Sprite dpadUp;
+    public Sprite dpadDown;
+    public Sprite dpadLeft;
+    public Sprite dpadRight;
+    public Sprite leftBumper;
+    public Sprite rightBumper;
+    public Sprite leftTrigger;
+    public Sprite rightTrigger;
+    public Sprite start;
+    public Sprite select;
+
+    
+
+    InputAction assignedInput;
+    InputBinding assignedBinding;
+    PlayerInput player;
+
+    public Sprite[] iconArray
+    {
+        get
+        {
+            return new Sprite[]
+            {
+                mouseLeft,
+                mouseRight,
+                mouseMiddle,
+                mouseMove,
+                mouseScrollWheel,
+                leftStickMove,
+                rightStickMove,
+                leftStickClick,
+                rightStickClick,
+                faceButtonNorth,
+                faceButtonSouth,
+                faceButtonEast,
+                faceButtonWest,
+                dpadUp,
+                dpadDown,
+                dpadLeft,
+                dpadRight,
+                leftBumper,
+                rightBumper,
+                leftTrigger,
+                rightTrigger,
+                start,
+                select,
+            };
+        }
+    }
+    public static readonly List<string> inputStrings = new List<string>(new string[]
+    {
+        "<Mouse>/leftButton",
+        "<Mouse>/rightButton",
+        "<Mouse>/middleButton",
+        "<Pointer>/delta",
+        "<Mouse>/scroll/y",
+        "<Gamepad>/leftStick",
+        "<Gamepad>/rightStick",
+        "<Gamepad>/leftStickPress",
+        "<Gamepad>/rightStickPress",
+        "<Gamepad>/buttonNorth",
+        "<Gamepad>/buttonSouth",
+        "<Gamepad>/buttonEast",
+        "<Gamepad>/buttonWest",
+        "<Gamepad>/dpad/up",
+        "<Gamepad>/dpad/down",
+        "<Gamepad>/dpad/left",
+        "<Gamepad>/dpad/right",
+        "<Gamepad>/leftShoulder",
+        "<Gamepad>/rightShoulder",
+        "<Gamepad>/leftTrigger",
+        "<Gamepad>/rightTrigger",
+        "<Gamepad>/start",
+        "<Gamepad>/select",
+    });
+
+    public void AssignAction(InputAction newInput, PlayerInput newPlayer)
+    {
+        assignedInput = newInput;
+        player = newPlayer;
+        gameObject.SetActive(true);
+        Update();
+    }
+
+    private void Update()
+    {
+        if (assignedInput == null || player == null)
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+
+        // Set up some kind of code so this code updates automatically when the input scheme changes
+
+        // Figure out the player's current input scheme
+        string currentPlayerInputScheme = player.currentControlScheme;
+        for (int i = 0; i < assignedInput.bindings.Count; i++)
+        {
+            // Check if an available binding matches the current control scheme
+            InputBinding b = assignedInput.bindings[i];
+            //Debug.Log(b.groups + ", " + b.path + ", " + currentPlayerInputScheme);
+            if (assignedBinding != b && b.groups.Contains(currentPlayerInputScheme))
+            {
+                assignedBinding = b;
+                Refresh(assignedBinding);
+                return;
+            }
+        }
+        // Reaching this point means a binding matching the current scheme was not found
+        // Disable button prompt
+        gameObject.SetActive(false);
+    }
+
+    public void Refresh(InputBinding binding)
+    {
+        string path = binding.effectivePath;
+
+        int index = inputStrings.IndexOf(path);
+        if (index >= 0 && index < iconArray.Length && iconArray[index] != null) // If index is valid, and the display name has a matching name in the list AND there's a corresponding sprite for the index
+        {
+            graphic.sprite = iconArray[index];
+            keyName.text = "";
+        }
+        else
+        {
+            // Default to key graphic and display name of binding
+            // Cut off everything before the binding name
+            int lastSlashBeforeName = path.LastIndexOf("/");
+            string displayName = path.Remove(0, lastSlashBeforeName + 1);
+            // Capitalise binding name
+
+            //Debug.Log(displayName);
+            keyName.text = displayName;
+            graphic.sprite = keyboardKey;
+        }
+
+        gameObject.SetActive(true);
+    }
+
 
     /*
-    public void Refresh(CustomInput.Button prompt)
-    {
-        SetMappingGraphic(prompt.keyboardMapping);
-    }
-
-    void SetMappingGraphic(CustomInput.ControllerButton button)
-    {
-        string path = "Graphics/Controller Buttons/ButtonPrompt_";
-        path += button.ToString();
-        graphic.sprite = Resources.Load<Sprite>(path);
-
-        Rect r = graphic.rectTransform.rect;
-        r.width = singleKeyDimensions.x;
-        r.height = singleKeyDimensions.y;
-        graphic.SetClipRect(r, true);
-        keyName.text = "";
-    }
-    */
     void SetMappingGraphic(KeyCode key)
     {
         int i;
@@ -67,7 +202,7 @@ public class GUIButtonPrompt : MonoBehaviour
         r.width = singleKeyDimensions.x;
         r.height = singleKeyDimensions.y;
         graphic.SetClipRect(r, true);
-        graphic.sprite = keyBackground;
+        graphic.sprite = keyboardKey;
         keyName.text = key.ToString();//System.Enum.GetName(typeof(KeyCode), key);
     }
     static readonly KeyCode[] slightlyWider = new KeyCode[]
@@ -96,4 +231,5 @@ public class GUIButtonPrompt : MonoBehaviour
         KeyCode.KeypadPlus,
         KeyCode.KeypadEnter,
     };
+    */
 }
