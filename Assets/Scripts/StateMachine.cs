88b8e682d2be2eee15c6e39b3275f37b9c14bcd5
Marcus Mouyat <marcus@mouyat.com>
2022-01-28 13:17:14 +1100

    SubStateMachine and MultiState classes
    I created two new classes deriving from StateMachine.State.
    The first, MultiState, includes a list of other states, and will run them all using for loops to activate their functions. This allows my state machine to easily run multiple states at a time using a single transition.
    The second, SubStateMachine, as the name may suggest, should act like a state machine within a state machine, with its own set of states and transitions. I can have the base state machine switch to a different set of behaviour that can include multiple state machines, switching between them as necessary.
    MultiState and SubStateMachine classes can be nested inside each other, allowing for an enormous level of flexibility when designing enemy behaviours. These classes have not been tested extensively, but I am pleased with how they are working so far.

--- Assets/Scripts/StateMachine.cs
+++ Assets/Scripts/StateMachine.cs
@@ -96,6 +96,124 @@ public class StateMachine : MonoBehaviour
     }
     #endregion
 
+    /// <summary>
+    /// Runs multiple states simultaneously.
+    /// </summary>
+    public class MultiState : StateMachine.State
+    {
+        public List<StateMachine.State> allStates = new List<StateMachine.State>();
+        public override void Enter(StateMachine controller)
+        {
+            for (int i = 0; i < allStates.Count; i++)
+            {
+                allStates[i].Enter(controller);
+            }
+        }
+        public override void Update(StateMachine controller)
+        {
+            for (int i = 0; i < allStates.Count; i++)
+            {
+                allStates[i].Update(controller);
+            }
+        }
+        public override void LateUpdate(StateMachine controller)
+        {
+            for (int i = 0; i < allStates.Count; i++)
+            {
+                allStates[i].LateUpdate(controller);
+            }
+        }
+        public override void FixedUpdate(StateMachine controller)
+        {
+            for (int i = 0; i < allStates.Count; i++)
+            {
+                allStates[i].FixedUpdate(controller);
+            }
+        }
+        public override void Exit(StateMachine controller)
+        {
+            for (int i = 0; i < allStates.Count; i++)
+            {
+                allStates[i].Exit(controller);
+            }
+        }
+    }
+    /// <summary>
+    /// Adds a secondary layer of states with their own transitions.
+    /// </summary>
+    public class SubStateMachine : StateMachine.State
+    {
+        public List<StateMachine.State> allStates = new List<StateMachine.State>();
+        public List<StateMachine.Transition> allTransitions = new List<StateMachine.Transition>();
+        int currentStateIndex = 0;
+        public StateMachine.State currentState
+        {
+            get
+            {
+                if (allStates.Count <= 0 || currentStateIndex < 0 || currentStateIndex > allStates.Count - 1)
+                {
+                    return null;
+                }
+                return allStates[currentStateIndex];
+            }
+            set
+            {
+                currentStateIndex = allStates.IndexOf(value);
+            }
+        }
+
+        public override void Enter(StateMachine controller)
+        {
+            currentState?.Enter(controller);
+        }
+        public override void Update(StateMachine controller)
+        {
+            if (currentState == null)
+            {
+                return;
+            }
+
+            for (int i = 0; i < allTransitions.Count; i++)
+            {
+                // If transition can happen from the current state (or from anywhere)
+                bool transitionCanOccur = allTransitions[i].from == currentState || allTransitions[i].from == null;
+                if (transitionCanOccur && allTransitions[i].conditions.Invoke() == true)
+                {
+                    currentState?.Exit(controller);
+                    currentState = allTransitions[i].to;
+                    currentState.Enter(controller);
+                }
+            }
+
+            currentState?.Update(controller);
+        }
+        public override void LateUpdate(StateMachine controller)
+        {
+            currentState?.LateUpdate(controller);
+        }
+        public override void FixedUpdate(StateMachine controller)
+        {
+            currentState?.FixedUpdate(controller);
+        }
+        public override void Exit(StateMachine controller)
+        {
+            currentState?.Exit(controller);
+        }
+
+        public void AddState(StateMachine.State newState, bool setAsDefault = false)
+        {
+            allStates.Add(newState);
+            if (setAsDefault)
+            {
+                currentState = newState;
+            }
+        }
+        public void AddTransition(StateMachine.State from, StateMachine.State to, Func<bool> conditions)
+        {
+            allTransitions.Add(new StateMachine.Transition(from, to, conditions));
+        }
+    }
+
     [System.Serializable]
     public class TestState : State
     {
