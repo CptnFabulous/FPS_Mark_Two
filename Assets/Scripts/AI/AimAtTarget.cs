ce10852c6122a2517d375a6378b0ac7d27741a54
Marcus Mouyat <marcus@mouyat.com>
2022-01-28 14:45:36 +1100

    Implementing AIAttack into AimAtTarget
    I had to overhaul much of my AimAtTarget class in order to make it work with AIAttack. Most if it was rather dry technical stuff, but the major changes are listed below:
    Added a better check for determining if the AI is successfully aiming at a target. Since the aiming code already uses bounds to determine the centre of mass to aim at, the check will use the dimensions of the enemy’s volume to determine an acceptable aim threshold. If the player is already aiming, an aim break threshold value is added on to give the aim a degree of ‘stickiness’.
    Tweaked code for determining the point in space to aim at, and whether the AI is successfully aiming at a target, to work off two virtual functions that can be altered in inherited classes. This is so if I need an enemy that attacks in a way that’s very different from simply requiring line of sight, I can make it easy for the enemy to aim towards the right target.
    Removed UnityEvents that occurred when acquiring aim, while aim was acquired and when aim was broken, since I was not using them for anything.

--- Assets/Scripts/AI/AimAtTarget.cs
+++ Assets/Scripts/AI/AimAtTarget.cs
@@ -6,86 +6,69 @@ using UnityEngine.Events;
 [System.Serializable]
 public class AimAtTarget : AIAction
 {
-    public AIAim.AimValues stats;
+    public AIAttack attack;
 
+    public AIAim.AimValues stats;
     public float aimLockThreshold;
     public float aimBreakThreshold;
-    public UnityEvent onTargetAcquired;
-    public UnityEvent whileTargetAcquired;
-    public UnityEvent onTargetLost;
-
-    public bool TargetAcquired
+    public virtual Vector3 GetTargetPoint()
     {
-        get
-        {
-            return targetLocked;
-        }
-        set
-        {
-            if (value != targetLocked)
-            {
-                if (value == true)
-                {
-                    onTargetAcquired.Invoke();
-                }
-                else
-                {
-                    onTargetLost.Invoke();
-                }
-            }
-            targetLocked = value;
-        }
+        Bounds targetBounds = CombatAI.target.health.HitboxBounds;
+        return targetBounds.center;
     }
-    bool targetLocked;
-    public Vector3 targetPoint { get; private set; }
+    public virtual bool CheckTargetStatus()
+    {
+        Bounds targetBounds = CombatAI.target.health.HitboxBounds;
+        float threshold = Mathf.Min(MiscFunctions.Vector3Array(targetBounds.extents));
+        if (TargetAcquired)
+        {
+            // Aim threshold is larger if a target is already acquired, for a bit of sluggishness in the enemy realising they've lost their aim
+            threshold += aimBreakThreshold;
+        }
+
+        return AI.aiming.LookCheckDistance(AI.aiming.AimDirection, TargetPoint, threshold);
+    }
+    public Vector3 TargetPoint { get; private set; }
+    public bool TargetAcquired { get; private set; }
+
 
     public override void Enter(StateMachine controller)
     {
         base.Enter(controller);
 
-        AimData.Stats = stats;
+        AimData.Stats = stats; // Set new stats
+        attack.behaviourUsingThis = this; // Assign a reference to the attack being triggered by this behaviour
     }
-
     public override void Update(StateMachine controller)
     {
         // Checks if it's presently possible to aim at the target
         bool lineOfSightPossible = LineOfSightCheck(AimData.LookOrigin, CombatAI.target.health.HitboxColliders, stats.lookDetection, stats.diameterForUnobstructedSight, Character.health.HitboxColliders);
         if (lineOfSightPossible)
         {
-            Bounds targetBounds = CombatAI.target.health.HitboxBounds;
-            targetPoint = targetBounds.center;
+            TargetPoint = GetTargetPoint(); // Determine current point in world space to aim at
             AimData.RotateLookTowards(TargetPoint); // Shift aim towards target point
+            TargetAcquired = CheckTargetStatus(); // Check if target is close enough to be within aim threshold
 
-            //targetAcquired = AimData.LookCheckAngle(AimData.AimDirection, targetPoint, threshold);
-            //float distanceLockThreshold = Mathf.Min(/* axes of targetBounds.extents.magnitude */);
-            float threshold = TargetAcquired ? aimBreakThreshold : aimLockThreshold;
-            TargetAcquired = AimData.LookCheckDistance(AimData.AimDirection, targetPoint, threshold);
-            if (TargetAcquired)
-            {
-                whileTargetAcquired.Invoke();
-            }
-            AimData.LookInNeutralDirection();
         }
         else // If not, return to neutral pose until target is found again
         {
-            TargetAcquired = false; // Enemy obviously doesn't have a target if line of sight is broken
+            if (TargetAcquired)
+            {
+                TargetAcquired = false; // Enemy obviously doesn't have a target if line of sight is broken
+                attack.End();
+            }
+
+            AimData.LookInNeutralDirection();
         }
-        
 
-
-
-        
+        // If aim is on target, attack
+        if (TargetAcquired)
+        {
+            attack.StartSequence();
+        }
     }
-
-    public void OnTargetAcquired()
+    public override void Exit(StateMachine controller)
     {
-        
-    }
-
-    public override void LateUpdate(StateMachine controller)
-    {
-        Color rayColour = TargetAcquired ? Color.green : Color.red;
-        Debug.DrawRay(AimData.LookOrigin, AimData.AimDirection * Vector3.Distance(AimData.LookOrigin, targetPoint), rayColour);
-        Debug.DrawRay(AimData.LookOrigin, AimData.LookDirection * Vector3.Distance(AimData.LookOrigin, targetPoint), Color.white);
+        attack.CancelSequence();
     }
 }
