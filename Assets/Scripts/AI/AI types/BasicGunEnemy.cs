569c55971d4c21a2028049527d40c996642b3c84
Marcus Mouyat <marcus@mouyat.com>
2022-01-30 12:05:30 +1100

    Removed references to test states from BasicGunEnemy

--- Assets/Scripts/AI/AI types/BasicGunEnemy.cs
+++ Assets/Scripts/AI/AI types/BasicGunEnemy.cs
@@ -8,22 +8,9 @@ public class BasicGunEnemy : Combatant
     public EngageTargetAtDistance engagementMovement;
     public AimAtTarget targetingStats;
 
-    
-
-    [Header("Testing stuff")]
-    public StateMachine.TestState firstState;
-    public StateMachine.TestState secondState;
-
     public override void Awake()
     {
-
-
         base.Awake();
-        //movementStates.AddTransition(firstState, secondState, firstState.TimeLimitExceeded());
-        //movementStates.AddTransition(secondState, firstState, secondState.TimeLimitExceeded());
-
-        //movementStates.AddState(firstState, true);
-        //movementStates.AddState(secondState);
 
         inCombat.allStates.Add(engagementMovement);
         inCombat.allStates.Add(targetingStats);
