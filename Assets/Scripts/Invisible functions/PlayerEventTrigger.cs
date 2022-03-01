9a88bd80b4a1494e632f2e4276078b2d85c90812
Marcus Mouyat <marcus@mouyat.com>
2022-03-01 16:16:27 +1100

    PlayerEventTrigger class to activate game events when the player walks into a certain area

---
+++ Assets/Scripts/Invisible functions/PlayerEventTrigger.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerEventTrigger : MonoBehaviour
+{
+    public UnityEvent<Player> onEnter;
+
+    Collider collider;
+    Rigidbody rigidbody;
+
+    private void Awake()
+    {
+        collider = GetComponent<Collider>();
+        collider.isTrigger = true;
+        rigidbody = GetComponent<Rigidbody>();
+        if (rigidbody == null)
+        {
+            rigidbody = gameObject.AddComponent<Rigidbody>();
+        }
+        rigidbody.isKinematic = true;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Player p = other.GetComponentInParent<Player>();
+        if (p != null)
+        {
+            Debug.Log(p);
+            onEnter.Invoke(p);
+        }
+    }
+}
