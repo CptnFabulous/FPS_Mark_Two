2dc99d2856e072bf008b56c133b3d4fc7149462d
Marcus Mouyat <marcus@mouyat.com>
2022-04-01 13:05:05 +1100

    Custom inspector data for ActionExecutor hierarchy
    I made a new custom editor that shows the hierarchy of action states set up in an ActionExecutor, as well as which ones are currently running. This makes visualising and debugging behaviours much easier.

--- Assets/Scripts/ActionExecutor.cs
+++ Assets/Scripts/ActionExecutor.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 /// <summary>
 /// A MonoBehaviour class designed to have an action or switching group of actions added to it.
@@ -123,5 +124,113 @@ public class MultiAction : Action
         {
             allActions[i].LateLoop();
         }
+    }
+}
+
+[CustomEditor(typeof(ActionExecutor))]
+public class ActionExecutorEditor : Editor
+{
+    ActionExecutor objectBeingEdited;
+    
+    GUIStyle inactiveStyle;
+    GUIStyle activeStyle;
+    GUIStyle CorrectStyle(bool active) => active ? activeStyle : inactiveStyle;
+
+    private void Awake()
+    {
+        inactiveStyle = new GUIStyle();
+        inactiveStyle.normal.textColor = Color.white;
+        activeStyle = new GUIStyle();
+        activeStyle.normal.textColor = Color.green;
+    }
+
+    private void OnEnable()
+    {
+        objectBeingEdited = serializedObject.targetObject as ActionExecutor;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        if (objectBeingEdited.baseAction != null)
+        {
+            ListAction(objectBeingEdited.baseAction, true, 0);
+        }
+        else
+        {
+            EditorGUILayout.HelpBox("No action is assigned.", MessageType.Warning);
+        }
+    }
+
+    public void ListAction(Action action, bool active, int layer)
+    {
+        if (action as MultiAction != null)
+        {
+            ListAction(action as MultiAction, active, layer);
+        }
+        else if (action as PriorityActionController != null)
+        {
+            ListAction(action as PriorityActionController, active, layer);
+        }
+        else if (action as FSM != null)
+        {
+            ListAction(action as FSM, active, layer);
+        }
+        else
+        {
+            EditorGUILayout.LabelField(Spacing(layer) + action.ToString(), CorrectStyle(active));
+        }
+    }
+    public void ListAction(MultiAction controller, bool active, int layer)
+    {
+        if (EditorGUILayout.Foldout(true, Spacing(layer) + controller.ToString(), CorrectStyle(active)) == false) return;
+
+        for (int i = 0; i < controller.allActions.Count; i++)
+        {
+            ListAction(controller.allActions[i], active, layer); // For some reason with this function I don't need to add 1 to the layer. Doing so causes the text to be shifted too far.
+        }
+    }
+    public void ListAction(PriorityActionController controller, bool active, int layer)
+    {
+        if (EditorGUILayout.Foldout(true, Spacing(layer) + controller.ToString(), CorrectStyle(active)) == false) return;
+
+        bool activeInController;
+        for (int i = 0; i < controller.allActions.Count; i++)
+        {
+            activeInController = active && controller.CurrentAction == controller.allActions[i].action;
+            ListAction(controller.allActions[i].action, activeInController, layer + 1);
+        }
+        if (controller.defaultAction != null)
+        {
+            activeInController = active && controller.CurrentAction == controller.defaultAction;
+            ListAction(controller.defaultAction, activeInController, layer + 1);
+        }
+    }
+    public void ListAction(FSM controller, bool active, int layer)
+    {
+        if (EditorGUILayout.Foldout(true, Spacing(layer) + controller.ToString(), CorrectStyle(active)) == false) return;
+
+        for (int i = 0; i < controller.allStates.Count; i++)
+        {
+            bool activeInController = active && controller.currentState == controller.allStates[i];
+            ListAction(controller.allStates[i], activeInController, layer + 1);
+        }
+    }
+
+    string Spacing(int numberOfLayers)
+    {
+        string s = "";
+
+        // Adds a number of spacings equal to the number of layers minus one, so the last one can be an arrow indicator
+        for (int i = 1; i < numberOfLayers; i++)
+        {
+            s += "    ";
+        }
+        // Adds the arrow indicator
+        if (numberOfLayers > 0)
+        {
+            s += " L> ";
+        }
+
+        return s;
     }
 }
