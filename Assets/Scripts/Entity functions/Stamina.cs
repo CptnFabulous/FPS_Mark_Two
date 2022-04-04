ed4ceac036b4b4ec6903b0d62256d5d79e7ed01e
Marcus Mouyat <marcus@mouyat.com>
2022-04-04 17:38:42 +1000

    Changed Stamina class to require Health class
    This was necessary since a Stamina class cannot run without a Health class present on the same object.

--- Assets/Scripts/Entity functions/Stamina.cs
+++ Assets/Scripts/Entity functions/Stamina.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(Health))]
 public class Stamina : MonoBehaviour
 {
     public Resource stamina = new Resource(25, 25, 5);
