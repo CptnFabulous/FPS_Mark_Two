2174b78f107158e584370a6c2485e6ba4d167a28
Marcus Mouyat <marcus@mouyat.com>
2022-04-07 13:37:52 +1000

    Player guns now transmit AttackMessages when firing.
    Now other objects in the scene can be aware of when the player is attacking, to respond accordingly.

--- Assets/Scripts/Player Weapons/GunFireController.cs
+++ Assets/Scripts/Player Weapons/GunFireController.cs
@@ -6,6 +6,7 @@ public class GunFireController : MonoBehaviour
 {
     public float roundsPerMinute = 600;
     public int maxBurst = 1;
+    public float messageDelay = 1;
 
     public float ShotDelay => 60 / roundsPerMinute;
     public bool CanBurst(int numberOfShots) => numberOfShots < maxBurst || maxBurst <= 0;
@@ -19,9 +20,24 @@ public class GunFireController : MonoBehaviour
     {
         InBurst = true;
         int shotsInBurst = 0;
-        
+
+        // last message time is set to negative infinity so that the message is always ran on the first shot
+        float timeOfLastMessage = Mathf.NegativeInfinity;
+
         while (CanBurst(shotsInBurst) && mode.PrimaryHeld && mode.CanShoot())
         {
+            // Transmit telegraph message to AI, if it's the first shot or enough time has passed since the previous message transmission
+            if (Time.time - timeOfLastMessage > messageDelay)
+            {
+                float spread = mode.stats.shotSpread + mode.User.standingAccuracy;
+                DirectionalAttackMessage newMessage = new DirectionalAttackMessage(mode.User.controller, mode.User.aimAxis.position, mode.User.AimDirection, mode.stats.range, spread, mode.stats.projectilePrefab.detection);
+                Notification<AttackMessage>.Transmit(newMessage);
+                //Debug.Log(mode.User.controller + " is firing " + mode.attachedTo + " in its " + mode + " mode, on frame " + Time.frameCount + "!");
+
+                timeOfLastMessage = Time.time; // Resets time
+            }
+
+            // Fire shot and increment burst timer
             mode.SingleShot();
             shotsInBurst++;
             
