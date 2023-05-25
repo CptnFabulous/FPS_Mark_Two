ff9db8cffcab570af996684e4b734aa5983c54a7
Marcus Mouyat <marcus@mouyat.com>
2023-05-25 15:51:53 +1000

    RadialMenu - cleaned up class and added 'AddVisualEffect()' function for including extra visuals
    The new function will be for additional graphics that need to be added at certain points in the menu, such as dividers.

--- Assets/Scripts/Player/Heads-up display/RadialMenu.cs
+++ Assets/Scripts/Player/Heads-up display/RadialMenu.cs
@@ -1,71 +1,44 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.Events;
-using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class RadialMenu : MonoBehaviour
 {
-    [Header("Stats")]
-    public Image[] options;
+    [SerializeField] Image optionPrefab;
+    [SerializeField] bool optionRotationsMatchAngle;
+    //public float angleOffset;
+    [SerializeField] RectTransform selectorAxis;
+
+    public UnityEvent<int> onValueChanged;
     public UnityEvent<int> onValueConfirmed;
 
-    public int Value
+
+
+    CanvasGroup elements;
+    RectTransform[] options = new RectTransform[0];
+    List<RectTransform> visualElements = new List<RectTransform>();
+
+    int index;
+    Vector2 cursorDirection;
+
+    public bool active { get; private set; }
+    public int value
     {
-        get
-        {
-            return index;
-        }
+        get => index;
         set
         {
             value = MiscFunctions.InverseClamp(value, 0, options.Length - 1);
-            if (index != value) // Only perform updating code if value is changed
-            {
-                index = value;
-                onValueChanged.Invoke(index);
-            }
+            if (index == value) return;// Only perform updating code if value is changed
+
+            index = value;
+            onValueChanged.Invoke(index);
         }
     }
-    public bool OptionsPresent
-    {
-        get
-        {
-            return options.Length > 0;
-        }
-    }
+    public bool optionsPresent => options != null && options.Length > 0;
+    float radius => Vector2.Distance(optionPrefab.rectTransform.anchoredPosition, Vector2.zero);
+    float segmentSize => optionsPresent ? (360 / options.Length) : 360;
     
-    [Header("Setup")]
-    public Image optionPrefab;
-    public bool rotateOptions;
-    //public float angleOffset;
-    public RectTransform selectorAxis;
-    public UnityEvent<int> onValueChanged;
-
-    CanvasGroup elements;
-    public bool active { get; private set; }
-    int index;
-    Vector2 cursorDirection;
-    float SegmentSize
-    {
-        get
-        {
-            return 360 / options.Length;
-        }
-    }
-    float SelectionAngle
-    {
-        get
-        {
-            float f = Vector2.SignedAngle(cursorDirection, Vector2.up);
-            if (f < 0)
-            {
-                f += 360;
-            }
-            return f;
-        }
-    }
-
     private void Awake()
     {
         elements = GetComponent<CanvasGroup>();
@@ -80,48 +53,71 @@ public class RadialMenu : MonoBehaviour
         elements.blocksRaycasts = active;
     }
 
-    #region Updating and input
+    #region Setup
     /// <summary>
-    /// Populates the radial menu with a series of options
+    /// Populates the radial menu with a series of options.
     /// </summary>
     /// <param name="icons"></param>
     public void Refresh(Sprite[] icons)
     {
-        // Destroy existing options icons
-        // Implementing code to save and repurpose current ones might theoretically improve performance, but I'll wait until it actually causes problems.
-        for (int i = 0; i < options.Length; i++)
-        {
-            if (options[i] != null)
-            {
-                Destroy(options[i].gameObject);
-            }
-        }
+        Clear();
 
-        // New array of options icons
-        options = new Image[icons.Length];
+        options = new RectTransform[icons.Length];
         for (int i = 0; i < options.Length; i++)
         {
-            // Establishes rotation relative to centre, and position to spawn object in
-            Quaternion rotation = Quaternion.Euler(0, 0, -SegmentSize * i);
-            Vector3 position = rotation * Vector3.up * Vector2.Distance(optionPrefab.rectTransform.anchoredPosition, Vector2.zero);
-            options[i] = Instantiate(optionPrefab, transform); // Spawns icon
-            options[i].gameObject.SetActive(true); // Enables icon since the prefab is hidden
-            options[i].sprite = icons[i]; // Assigns sprite
-            options[i].rectTransform.anchoredPosition = position; // Sets position
-            // If rotateOptions is enabled, rotates icon around centre axis, otherwise sets to zero
-            options[i].rectTransform.localRotation = rotateOptions ? rotation : Quaternion.identity;
+            Image newOption = Instantiate(optionPrefab);
+            newOption.sprite = icons[i];
+            AddVisualEffect(newOption.rectTransform, i, 1, optionRotationsMatchAngle);
+            options[i] = newOption.rectTransform;
         }
     }
+    /// <summary>
+    /// Adds another visual element to the radial menu.
+    /// </summary>
+    /// <param name="objectTransform">The visual element to show.</param>
+    /// <param name="orderIndex">Corresponds with the index of each option, but as a float so you can put things before or after.</param>
+    /// <param name="distance"></param>
+    /// <param name="preserveAngle"></param>
+    public void AddVisualEffect(RectTransform objectTransform, float orderIndex, float distance, bool preserveAngle, bool addBehind = false)
+    {
+        // Establishes rotation relative to centre, and position to spawn object in
+        Quaternion rotation = Quaternion.Euler(0, 0, -segmentSize * orderIndex);
+        Vector3 position = radius * distance * (rotation * Vector3.up);
+
+        // Parents the object transform and sets up its position and rotation
+        objectTransform.SetParent(transform);
+        if (addBehind)
+        {
+            objectTransform.SetAsFirstSibling();
+        }
+        objectTransform.anchoredPosition = position; // Sets position
+        objectTransform.localRotation = preserveAngle ? rotation : Quaternion.identity; // If preserveAngle is false, object retains an upright rotation
+
+        // Activates the visual and ensures the radial menu recognises it.
+        visualElements.Add(objectTransform);
+        objectTransform.gameObject.SetActive(true);
+    }
+    void Clear()
+    {
+        // Clear options and destroy all instantiated visuals
+        // Implementing code to save and repurpose current ones might theoretically improve performance, but I'll wait until it actually causes problems.
+        foreach (RectTransform visual in visualElements) Destroy(visual.gameObject);
+        visualElements.Clear();
+
+        options = new RectTransform[0]; // Set length to zero
+    }
+    #endregion
+
+
+    #region Controls
     /// <summary>
     /// Inputs a 2D vector based off a mouse or analog stick input, to update the selection angle
     /// </summary>
     /// <param name="value"></param>
     public void InputDirection(Vector2 value)
     {
-        if (!active)
-        {
-            return;
-        }
+        if (!active) return;
+
         // Input mouse/analog stick movement
         cursorDirection += value;
         if (cursorDirection.magnitude > 1)
@@ -129,8 +125,11 @@ public class RadialMenu : MonoBehaviour
             cursorDirection.Normalize();
         }
 
-        Value = Mathf.RoundToInt(SelectionAngle / SegmentSize);
-        selectorAxis.localRotation = Quaternion.Euler(0, 0, -SelectionAngle);
+        float selectionAngle = Vector2.SignedAngle(cursorDirection, Vector2.up);
+        if (selectionAngle < 0) selectionAngle += 360;
+
+        this.value = Mathf.RoundToInt(selectionAngle / segmentSize);
+        selectorAxis.localRotation = Quaternion.Euler(0, 0, -selectionAngle);
     }
 
     /// <summary>
@@ -139,16 +138,13 @@ public class RadialMenu : MonoBehaviour
     /// <param name="index"></param>
     public void EnterMenu(int newIndex)
     {
-        if (options.Length <= 0)
-        {
-            return;
-        }
+        if (optionsPresent == false) return;
 
         index = newIndex;
         onValueChanged.Invoke(index);
         
         cursorDirection = Vector2.zero;
-        selectorAxis.localRotation = Quaternion.Euler(0, 0, -SegmentSize * index);
+        selectorAxis.localRotation = Quaternion.Euler(0, 0, -segmentSize * index);
         SetActiveState(true);
     }
     /// <summary>
@@ -157,15 +153,15 @@ public class RadialMenu : MonoBehaviour
     public void ExitMenu()
     {
         SetActiveState(false);
-        onValueConfirmed.Invoke(Value);
+        onValueConfirmed.Invoke(value);
     }
     #endregion
 
     #region Optional functions
     public void PositionHighlight(RectTransform selectionHighlight)
     {
-        selectionHighlight.anchoredPosition = options[Value].rectTransform.anchoredPosition;
-        selectionHighlight.rotation = options[Value].rectTransform.rotation;
+        selectionHighlight.anchoredPosition = options[value].anchoredPosition;
+        selectionHighlight.rotation = options[value].rotation;
     }
     #endregion
 }
