87998a4d406272ac1811dbcd6f0c1bb99b74c863
Marcus Mouyat <marcus@mouyat.com>
2023-09-28 19:50:34 +1000

    Removed custom state machine package as I'm not using it anymore

--- Assets/Scripts/AI Revision 2/TravelToDestination.cs
+++ Assets/Scripts/AI Revision 2/TravelToDestination.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-using CptnFabulous.StateMachines;
 
 public abstract class TravelToDestination : AIStateFunction
 {
