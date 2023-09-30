a91c4b1311dae0645e9c08f367f129f440d8f10c
Marcus Mouyat <marcus@mouyat.com>
2023-09-30 15:23:15 +1000

    TravelToDestination - removed a heap of code that's no longer being used

--- Assets/Scripts/AI Revision 2/TravelToDestination.cs
+++ Assets/Scripts/AI Revision 2/TravelToDestination.cs
@@ -17,25 +17,6 @@ public abstract class TravelToDestination : AIStateFunction
     // Avoiding an incoming attack
     // Flanking maneuver
 
-    public bool DestinationReached() => navMeshAgent.remainingDistance <= destinationThreshold;
-    /*
-    public override Status GetStatus()
-    {
-        NavMeshPath path = navMeshAgent.path;
-
-        bool viable = IsPathViable();
-        //Debug.Log($"{path != null}, {path.status}, {valid}");
-        if (path != null && path.status == NavMeshPathStatus.PathComplete && viable)
-        {
-            if (endStateOnceDestinationReached && DestinationReached())
-            {
-                return Status.Completed; // Agent has already reached destination
-            }
-            return Status.Active; // Agent is moving towards destination
-        }
-        return Status.Blocked; // Destination is either invalid or no longer viable
-    }
-    */
     protected virtual void OnEnable()
     {
         navMeshAgent.isStopped = false;
