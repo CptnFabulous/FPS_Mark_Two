61d58dc642b12290b765ff67718189cab087dc2d
Marcus Mouyat <marcus@mouyat.com>
2023-10-09 12:45:18 +1100

    SmokeCloud collider pooling
    Colliders generated for SmokeCloud are now childed to a central parent transform, and are selected from this pool to act as the colliders for any active SmokeClouds in the scene. This has two upsides:
    1. Smoke cloud objects can move around using rigidbody physics, and won't be affected by the positions of the smoke colliders
    2. Colliders are now shared between all smoke clouds, reducing the number that need to be spawned at runtime.

--- Assets/Scripts/Player Weapons/SmokeCloud.cs
+++ Assets/Scripts/Player Weapons/SmokeCloud.cs
@@ -4,45 +4,101 @@ using UnityEngine;
 
 public class SmokeCloud : MonoBehaviour
 {
-    public ParticleSystem particleSystem;
-    
+    [SerializeField] ParticleSystem particleSystem;
+
     ParticleSystem.Particle[] particleArray;
     SphereCollider[] colliderArray;
+
+    static Queue<SphereCollider> colliderPool; // Contains all inactive colliders
+    static Transform poolParent; // The parent for all colliders, so smoke plumes with rigidbody physics don't behave weirdly
+
+    public bool emitting
+    {
+        get => particleSystem.isEmitting;
+        set
+        {
+            if (value == emitting) return;
+
+            if (value)
+            {
+                particleSystem.Play();
+                enabled = true;
+            }
+            else
+            {
+                particleSystem.Stop();
+            }
+        }
+    }
 
     private void Awake()
     {
         int maxParticles = particleSystem.main.maxParticles;
         particleArray = new ParticleSystem.Particle[maxParticles];
         colliderArray = new SphereCollider[maxParticles];
+
+        // Set up object pool handlers (runs here to minimise the amount of checks)
+        if (colliderPool == null) colliderPool = new Queue<SphereCollider>();
+        if (poolParent == null)
+        {
+            poolParent = new GameObject("SmokeCloud collider pool parent").transform;
+            Object.DontDestroyOnLoad(poolParent);
+        }
     }
     void FixedUpdate()
     {
+        if (emitting == false && particleSystem.particleCount <= 0)
+        {
+            Debug.Log($"Disabling {this} as there is currently no smoke to manage");
+            enabled = false;
+            return;
+        }
+
+        // Obtains the current particle data, to assign the colliders to
         particleSystem.GetParticles(particleArray);
         for (int i = 0; i < colliderArray.Length; i++)
         {
-            // Check how many particles need to have colliders assigned
-            bool colliderRequired = i < particleSystem.particleCount;
-            ParticleSystem.Particle p = particleArray[i];
             SphereCollider c = colliderArray[i];
 
-            if (colliderRequired && c == null)
+            // If we've accounted for all the particles, place all excess colliders back in the pool
+            if (i >= particleSystem.particleCount)
             {
-                GameObject g = new GameObject("Smoke Collider #" + (i + 1));
-                g.transform.parent = particleSystem.transform;
-                g.layer = particleSystem.gameObject.layer;
-                c = g.AddComponent<SphereCollider>();
+                #region Remove excess collider from array and proceed to next entry
+                if (c != null)
+                {
+                    c.gameObject.SetActive(false);
+                    colliderPool.Enqueue(c);
+                    colliderArray[i] = null;
+                }
+                continue;
+                #endregion
+            }
+            else if (c == null) // If a collider is required but not assigned, get one
+            {
+                #region Get a collider
+                // Get one from the pool. If there aren't any more, create one.
+                if (colliderPool.Count > 0)
+                {
+                    c = colliderPool.Dequeue();
+                }
+                else
+                {
+                    GameObject g = new GameObject("Smoke Collider");
+                    g.transform.parent = poolParent;
+                    g.layer = particleSystem.gameObject.layer;
+                    c = g.AddComponent<SphereCollider>();
+                }
+
                 colliderArray[i] = c;
+                c.gameObject.layer = particleSystem.gameObject.layer;
+                c.gameObject.SetActive(true);
+                #endregion
             }
 
-            if (c != null)
-            {
-                c.gameObject.SetActive(colliderRequired);
-                if (colliderRequired)
-                {
-                    c.radius = p.GetCurrentSize(particleSystem) / 2;
-                    c.transform.position = p.position;
-                }
-            }
+            // Assign collider variables
+            ParticleSystem.Particle p = particleArray[i];
+            c.radius = p.GetCurrentSize(particleSystem) / 2;
+            c.transform.position = p.position;
         }
     }
 }
