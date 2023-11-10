23a1c328d60f8f0b6ba4fdbc82eb28f5a8cc6d46
Marcus Mouyat <marcus@mouyat.com>
2023-11-10 14:14:12 +1100

    Removed redundant weapon info code and assets from HeadsUpDisplay
    This functionality is now performed by the WeaponInfo and WeaponModeInfo classes, but I forgot to delete the old version of the feature.

--- Assets/Scripts/Player/Heads-up display/HeadsUpDisplay.cs
+++ Assets/Scripts/Player/Heads-up display/HeadsUpDisplay.cs
@@ -91,58 +91,6 @@ public class HeadsUpDisplay : MonoBehaviour
         damageEffects.Invoke();
     }
 
-    [Header("Weapons")]
-    public GameObject weaponInterface;
-    public ResourceMeter magazineMeter;
-    public ResourceMeter ammoReserve;
-    public Text weaponModeName;
-    public Image weaponModeIcon;
-    public void UpdateWeaponHUD(Weapon currentWeapon)
-    {
-        weaponInterface.gameObject.SetActive(currentWeapon != null);
-        if (currentWeapon == null) return;
-
-        WeaponMode mode = currentWeapon.CurrentMode;
-        weaponModeName.text = mode.name;
-        weaponModeIcon.sprite = mode.icon;
-
-
-
-
-
-
-        // Check if attack is a ranged mode
-        RangedAttack rangedAttack = mode as RangedAttack;
-        magazineMeter.gameObject.SetActive(rangedAttack != null);
-        ammoReserve.gameObject.SetActive(rangedAttack != null);
-
-        if (rangedAttack == null) return;
-
-        // If weapon consumes ammo, show reserve
-        bool consumesAmmo = rangedAttack.consumesAmmo;
-        ammoReserve.gameObject.SetActive(consumesAmmo);
-        if (consumesAmmo)
-        {
-            Resource remainingAmmo = controller.weapons.ammo.GetValues(rangedAttack.stats.ammoType);
-
-            if (rangedAttack.magazine != null) // If magazine is present, change ammo bar to show reserve excluding magazine amount
-            {
-                remainingAmmo.current -= rangedAttack.magazine.ammo.current;
-                remainingAmmo.max -= (int)rangedAttack.magazine.ammo.max;
-            }
-
-            ammoReserve.Refresh(remainingAmmo);
-        }
-
-        // If weapon has a magazine, show values
-        magazineMeter.gameObject.SetActive(rangedAttack.magazine != null);
-        if (rangedAttack.magazine != null)
-        {
-            magazineMeter.Refresh(rangedAttack.magazine.ammo);
-        }
-    }
-
-
     private void Awake()
     {
         canvas = GetComponent<Canvas>();
