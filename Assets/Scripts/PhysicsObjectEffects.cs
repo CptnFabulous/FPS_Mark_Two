da409c60ac7d85b384c236ae2037027e5f3b60b8
Marcus Mouyat <marcus@mouyat.com>
2024-04-18 18:14:36 +1000

    Cleaned up physics object collision code to eliminate redundant checks
    Checks now won't occur between objects that are directly parented to each other, and the system avoids using Collision.contacts, which is responsible for a lot of garbage.

--- Assets/Scripts/PhysicsObjectEffects.cs
+++ Assets/Scripts/PhysicsObjectEffects.cs
@@ -14,12 +14,24 @@ public class PhysicsObjectEffects : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        // Calculate multiplier for effects
         float force = collision.relativeVelocity.magnitude - minImpactForce;
         float forceRange = maxImpactForce - minImpactForce;
         float multiplier = force / forceRange;
 
-        foreach (ContactPoint contactPoint in collision.contacts)
+        // Cancel if threshold isn't met to bother displaying effects
+        if (multiplier <= 0) return;
+
+        // Play effect for each contact point
+        for (int i = 0; i < collision.contactCount; i++)
         {
+            ContactPoint contactPoint = collision.GetContact(i);
+
+            // Checks if one collider is not a parent of the other (check if this actually changes anything)
+            Transform a = contactPoint.thisCollider.transform;
+            Transform b = contactPoint.otherCollider.transform;
+            if (a.parent == b || b.parent == a) continue;
+
             impactEffect.Play(collision.collider.gameObject, rootEntity, contactPoint.point, contactPoint.normal, multiplier);
         }
     }
