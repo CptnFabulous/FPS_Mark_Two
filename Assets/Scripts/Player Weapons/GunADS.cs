c0e2b3825a43ee824f0edb08da4d7ff6d61bcad9
Marcus Mouyat <marcus@mouyat.com>
2024-04-28 10:54:45 +1000

    GunADS - moved functional and cosmetic lerping code into its own functions, so they can be set manually for cancelling actions

--- Assets/Scripts/Player Weapons/GunADS.cs
+++ Assets/Scripts/Player Weapons/GunADS.cs
@@ -104,6 +104,38 @@ public class GunADS : MonoBehaviour
             timer = Mathf.MoveTowards(timer, TargetValue, Time.deltaTime / transitionTime);
         }
 
+        LerpADS(timer);
+    }
+    private void LateUpdate()
+    {
+        if (notSetupProperly)
+        {
+            enabled = false;
+            return;
+        }
+
+        if (IsAiming || IsTransitioning)
+        {
+            LerpADSCosmetics(timer);
+        }
+    }
+    /*
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawRay(reticleAxis.position, -reticleAxis.forward * distanceBetweenReticleAxisAndHead);
+        if (modelOrientationTransform != null)
+        {
+            Debug.DrawRay(modelOrientationTransform.position, modelOrientationTransform.forward, new Color(1, 0.5f, 0));
+            Debug.DrawRay(modelOrientationTransform.position, modelOrientationTransform.up, new Color(1, 0.5f, 0));
+        }
+    }
+    */
+    /// <summary>
+    /// Lerps the FOV and camera direction between standard and ADS modes.
+    /// </summary>
+    /// <param name="timer"></param>
+    void LerpADS(float timer)
+    {
         // If a view camera and sight picture are not present, directly lerp the player's FOV instead
         if (!IsScope)
         {
@@ -117,57 +149,41 @@ public class GunADS : MonoBehaviour
         Vector3 cameraDirection = Vector3.Slerp(aimAxis.forward, userWeaponHandler.AimDirection, timer);
         upperBody.LookAt(upperBody.position + cameraDirection, aimAxis.up);
     }
-    private void LateUpdate()
+    /// <summary>
+    /// Lerps appropriate cosmetic features between standard and ADS modes.
+    /// </summary>
+    /// <param name="timer"></param>
+    void LerpADSCosmetics(float timer)
     {
-        if (notSetupProperly)
+        // Rotate gun so the reticle axis transform is parallel with the player's aim direction
+        Quaternion relativeRotation = MiscFunctions.FromToRotation(modelPivot.rotation, reticleAxis.rotation);
+        Quaternion rotation = lookControls.upperBody.rotation * Quaternion.Inverse(relativeRotation);
+        modelOrientationTransform.rotation = Quaternion.Lerp(hipFireOrientation.rotation, rotation, timer);
+
+        // If look sway values are greater than zero, run sway cosmetics
+        // This check exists to prevent unnecessary processing, and also to prevent division by zero causing weird errors
+        if (lookSwayDegrees != 0 && speedForMaxSway > 0)
         {
-            enabled = false;
-            return;
+            // Modify gun rotation by sway value
+            // Vector3.SmoothDamp is used on the euler angles for clean transitions.
+            // If the smoothdamp is applied directly to the gun rotation, it drags too far behind.
+            // Applying the base gun rotation straight then using smoothdamp on just the sway value is better at keeping the rotation within specified boundaries.
+            Quaternion localRotationVelocity = MiscFunctions.WorldToLocalRotation(lookControls.rotationVelocity, player.movement.transform);
+            float intensity = Mathf.Clamp01(localRotationVelocity.eulerAngles.magnitude / speedForMaxSway);
+            Vector3 swayAxes = new Vector3(localRotationVelocity.x, localRotationVelocity.y, 0); // Only record X and Y values to prevent awkward shifting
+            swayAxes = Vector3.Lerp(Vector3.zero, swayAxes.normalized * -lookSwayDegrees, intensity);
+            cosmeticSwayAxes = Vector3.SmoothDamp(cosmeticSwayAxes, swayAxes, ref cosmeticSwayAngularVelocity, swayUpdateTime);
+            modelOrientationTransform.localRotation *= Quaternion.Euler(cosmeticSwayAxes); // Apply sway on top of the regular rotation
         }
-        //Debug.Log($"{this}: running LateUpdate()");
 
-        if (IsAiming || IsTransitioning)
-        {
-            // Rotate gun so the reticle axis transform is parallel with the player's aim direction
-            Quaternion relativeRotation = MiscFunctions.FromToRotation(modelPivot.rotation, reticleAxis.rotation);
-            Quaternion rotation = lookControls.upperBody.rotation * Quaternion.Inverse(relativeRotation);
-            modelOrientationTransform.rotation = Quaternion.Lerp(hipFireOrientation.rotation, rotation, timer);
+        //viewingCamera.transform.rotation = Quaternion.LookRotation(player.movement.upperBody.transform.forward, player.movement.upperBody.transform.up);
 
-            // If look sway values are greater than zero, run sway cosmetics
-            // This check exists to prevent unnecessary processing, and also to prevent division by zero causing weird errors
-            if (lookSwayDegrees != 0 && speedForMaxSway > 0)
-            {
-                // Modify gun rotation by sway value
-                // Vector3.SmoothDamp is used on the euler angles for clean transitions.
-                // If the smoothdamp is applied directly to the gun rotation, it drags too far behind.
-                // Applying the base gun rotation straight then using smoothdamp on just the sway value is better at keeping the rotation within specified boundaries.
-                Quaternion localRotationVelocity = MiscFunctions.WorldToLocalRotation(lookControls.rotationVelocity, player.movement.transform);
-                float intensity = Mathf.Clamp01(localRotationVelocity.eulerAngles.magnitude / speedForMaxSway);
-                Vector3 swayAxes = new Vector3(localRotationVelocity.x, localRotationVelocity.y, 0); // Only record X and Y values to prevent awkward shifting
-                swayAxes = Vector3.Lerp(Vector3.zero, swayAxes.normalized * -lookSwayDegrees, intensity);
-                cosmeticSwayAxes = Vector3.SmoothDamp(cosmeticSwayAxes, swayAxes, ref cosmeticSwayAngularVelocity, swayUpdateTime);
-                modelOrientationTransform.localRotation *= Quaternion.Euler(cosmeticSwayAxes); // Apply sway on top of the regular rotation
-            }
-
-            //viewingCamera.transform.rotation = Quaternion.LookRotation(player.movement.upperBody.transform.forward, player.movement.upperBody.transform.up);
-
-            // Calculate position of weapon model so reticle lines up with aim direction, outwards by distanceBetweenReticleAxisAndHead
-            // This must be done after rotation, because rotating will change the relative position of the reticle axis
-            Vector3 reticleRelativeToModelTransform = reticleAxis.position - modelPivot.position;
-            Vector3 reticleRelativeToHead = lookControls.upperBody.forward * distanceBetweenReticleAxisAndHead;
-            Vector3 position = lookControls.upperBody.position - reticleRelativeToModelTransform + reticleRelativeToHead;
-            modelOrientationTransform.position = Vector3.Lerp(hipFireOrientation.position, position, timer);
-        }
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.DrawRay(reticleAxis.position, -reticleAxis.forward * distanceBetweenReticleAxisAndHead);
-        if (modelOrientationTransform != null)
-        {
-            Debug.DrawRay(modelOrientationTransform.position, modelOrientationTransform.forward, new Color(1, 0.5f, 0));
-            Debug.DrawRay(modelOrientationTransform.position, modelOrientationTransform.up, new Color(1, 0.5f, 0));
-        }
+        // Calculate position of weapon model so reticle lines up with aim direction, outwards by distanceBetweenReticleAxisAndHead
+        // This must be done after rotation, because rotating will change the relative position of the reticle axis
+        Vector3 reticleRelativeToModelTransform = reticleAxis.position - modelPivot.position;
+        Vector3 reticleRelativeToHead = lookControls.upperBody.forward * distanceBetweenReticleAxisAndHead;
+        Vector3 position = lookControls.upperBody.position - reticleRelativeToModelTransform + reticleRelativeToHead;
+        modelOrientationTransform.position = Vector3.Lerp(hipFireOrientation.position, position, timer);
     }
 
 }
