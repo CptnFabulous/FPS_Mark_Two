0062a3b18d57531adc0e6c36522d15284f3ea5f2
Marcus Mouyat <marcus@mouyat.com>
2024-05-22 14:02:01 +1000

    Refactoring of interaction function - GUIs are improved and the player no longer needs to directly look at an object to interact with it
    This is a little QOL effect so the player can interact with things more quickly in a fight, e.g. to pick up more ammo. That sort of thing shouldn't actually be difficult to do, so having it be finicky isn't 'challenge', it's just annoying.
    The new interaction system is also designed to work with my upcoming mechanic for picking up and throwing objects.

---
+++ Assets/Scripts/Player/InteractionHandler.cs
@@ -0,0 +1,134 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InteractionHandler : MonoBehaviour
+{
+    [Header("Detection")]
+    public float interactionRange = 2;
+    public float detectionAngle = 60;
+    public LayerMask detectionMask = ~0;
+
+    [Header("Player data")]
+    public Player player;
+    public Camera referenceCamera;
+    public SingleInput input;
+
+    [Header("Interaction")]
+    public UnityEvent<Player, Interactable> onInteract;
+    public UnityEvent<Player, Interactable> onInteractFailed;
+    public PropCarryingHandler objectCarrier;
+
+    List<Collider> collidersChecked = new List<Collider>();
+
+    public Interactable targetedInteractable { get; private set; }
+    public Rigidbody targetedPhysicsProp { get; private set; }
+    public RaycastHit hitData { get; private set; }
+
+    Vector3 aimOrigin => referenceCamera.transform.position;
+    Vector3 aimDirection => referenceCamera.transform.forward;
+
+    private void Awake()
+    {
+        input.onActionPerformed.AddListener((_) => AttemptInteraction());
+    }
+    private void Update()
+    {
+        // Reset values
+        targetedInteractable = null;
+        targetedPhysicsProp = null;
+        hitData = new RaycastHit();
+        collidersChecked.Clear();
+
+        // Obtain initial colliders
+        collidersChecked.AddRange(Physics.OverlapSphere(aimOrigin, interactionRange, detectionMask));
+
+        #region Sort by which is closest to the centre of the camera
+
+        // By sorting first we don't need to fully check every single option, only until we find a valid one
+        Vector2 aimScreenPoint = referenceCamera.WorldToScreenPoint(aimOrigin + aimDirection);
+        MiscFunctions.SortListWithOnePredicate(collidersChecked, (c) =>
+        {
+            // Get screen position of bounds centre, and compare it to the position of the player's reticle
+            // (use sqrMagnitude since we don't need the exact distance)
+            Vector2 screenPosition = referenceCamera.WorldToScreenPoint(c.bounds.center);
+            return (screenPosition - aimScreenPoint).sqrMagnitude;
+        });
+
+        #endregion
+
+        #region Get first entry that meets the criteria
+
+        foreach (Collider c in collidersChecked)
+        {
+            // Check for either an Interactable or Rigidbody, ignore if not found
+            Interactable i = c.GetComponentInParent<Interactable>();
+            Rigidbody rb = null;
+            if (objectCarrier != null)
+            {
+                rb = MiscFunctions.GetComponentInParentThatMeetsCriteria<Rigidbody>(c.transform, objectCarrier.CanPickUpObject);
+            }
+            if (i == null && rb == null) continue;
+
+            // Check if within field of view, ignore if not
+            if (Vector3.Angle(aimDirection, c.bounds.center - aimOrigin) > detectionAngle) continue;
+
+            // Check for line of sight, ignore if the raycast didn't hit the desired collider, or at all
+            Ray ray = new Ray(aimOrigin, c.bounds.center - aimOrigin);
+            if (Physics.Raycast(ray, out RaycastHit rh, interactionRange, detectionMask) == false) continue;
+            if (rh.collider != c) continue;
+
+            // We've found the closest interactable! Mark it as the highlighted one
+            targetedInteractable = i;
+            targetedPhysicsProp = rb;
+            hitData = rh;
+            break;
+        }
+
+        #endregion
+
+        // Do stuff with highlighted interactable
+        //Debug.Log($"Targeted = {targetedInteractable}, {targetedPhysicsProp}");
+    }
+
+
+
+
+    protected virtual void AttemptInteraction()
+    {
+        if (targetedInteractable != null)
+        {
+            ContextInteractionCheck(targetedInteractable);
+        }
+        /*
+        else if (targetedPhysicsProp != null)
+        {
+            PhysicsPropInteractionCheck(targetedPhysicsProp);
+        }
+        */
+        // TO DO: do ContextInteractionCheck if a context interactable is present, otherwise try PhysicsPropInteractionCheck
+    }
+    void ContextInteractionCheck(Interactable target)
+    {
+        if (target.CanInteract(player))
+        {
+            Debug.Log($"Interacting with {target}");
+            target.OnInteract(player);
+            onInteract.Invoke(player, target);
+        }
+        else
+        {
+            // Display some kind of 'on failed interaction' message (e.g. a noise)
+            Debug.Log($"Cannot interact with {target}");
+            onInteractFailed.Invoke(player, target);
+        }
+    }
+    /*
+    void PhysicsPropInteractionCheck(Rigidbody target)
+    {
+
+    }
+    */
+
+}
---
+++ Assets/Scripts/Player/InteractionWindow.cs
@@ -0,0 +1,129 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class InteractionWindow : MonoBehaviour
+{
+    public InteractionHandler following;
+
+    [Header("UI elements")]
+    public CanvasGroup canvasGroup;
+    public TMP_Text interactableName;
+    public TMP_Text action;
+    public GUIButtonPrompt prompt;
+    public Image progressBar;
+
+    [Header("Physics interaction")]
+    public string pickupText = "Pick up";
+    public string deniedText = "Too heavy";
+
+    public PropCarryingHandler objectCarrier => following.objectCarrier;
+
+    private void Awake()
+    {
+        Debug.Log("Awaking");
+        // Assign button prompt
+        PlayerInput input = following.player.controls;
+        InputAction action = following.input.action;
+        prompt.AssignAction(action, input);
+    }
+
+    private void LateUpdate()
+    {
+        bool somethingToInteractWith = following.targetedInteractable != null;
+        //bool somethingToPickUp = objectCarrier != null && following.targetedPhysicsProp != null;
+        bool somethingPresent = somethingToInteractWith/* || somethingToPickUp*/;
+        canvasGroup.alpha = somethingPresent ? 1 : 0;
+        if (!somethingPresent) return;
+        
+
+        // TO DO: have a bit here to gain the renderer/bounds and position the interaction window accordingly
+
+
+        if (somethingToInteractWith)
+        {
+            DisplayInteractable(following.targetedInteractable);
+        }
+        /*
+        else if (somethingToPickUp)
+        {
+            DisplayPhysicsProp(following.targetedPhysicsProp);
+        }
+        */
+    }
+
+
+    void DisplayInteractable(Interactable target)
+    {
+        bool canInteract = target.CanInteract(following.player);
+        float progress = target.Progress;
+
+        SetInteractability(canInteract);
+
+        // Set values
+        interactableName.text = target.name;
+        progressBar.fillAmount = progress;
+        if (progress > 0 && progress < 1)
+        {
+            action.text = target.inProgressMessage;
+        }
+        else
+        {
+            action.text = canInteract ? target.promptMessage : target.disabledMessage;
+        }
+    }
+    /*
+    void DisplayPhysicsProp(Rigidbody target)
+    {
+        // Set values
+        SetInteractability(true);
+        interactableName.text = target.name;
+        progressBar.fillAmount = 0;
+        action.text = "Pick up";
+    }
+    */
+
+
+    void SetInteractability(bool canInteract)
+    {
+        // Set active states of button prompt and status icon
+
+        prompt.inputDisabled = !canInteract;
+    }
+
+
+
+
+
+    /*
+    public static Rect BoundsToRect(Bounds bounds, Camera referenceCamera)
+    {
+        Vector2 startPos = referenceCamera.WorldToScreenPoint(bounds.min);
+        Rect final = new Rect(startPos, Vector2.zero); // Input the first value. If it starts at zero but no part of the bounds is inside zero, then the size will be inaccurate.
+        for (int x = -1; x < 2; x += 2)
+        {
+            for (int y = -1; y < 2; y += 2)
+            {
+                for (int z = -1; z < 2; z += 2)
+                {
+                    // Calculate each corner. Since x, y and z will always be either -1 or 1, multiplying the extents by every combination will result in every corner
+                    Vector3 corner = bounds.extents;
+                    corner.x *= x;
+                    corner.y *= y;
+                    corner.z *= z;
+                    corner += bounds.center;
+                    // Obtain the screen space, and expand the Rect to encompass it
+                    Vector2 screenPoint = referenceCamera.WorldToScreenPoint(corner);
+                    final.min = Vector2.Min(final.min, screenPoint);
+                    final.max = Vector2.Max(final.max, screenPoint);
+                }
+            }
+        }
+
+        return final;
+    }
+    */
+}
