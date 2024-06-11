41954a75bc60ad7f6d10f504af4a7a3c3a974288
Marcus Mouyat <marcus@mouyat.com>
2024-06-11 12:45:11 +1000

    PhysicsAffectedAI - fixed bugs with repositioning transform and ragdoll
    One bug involved the transform not matching the visual position of the ragdoll, which may have caused seemingly inaccurate position data.
    The other bug involved the ragdoll having the wrong rotation compared to the AI, which sometimes resulted in the enemy facing a different direction to what they appeared to be.
    Finally, I wanted to ensure the transform's rotation makes sense for when the AI deragdollises, for the purposes of 'standing up' animations.

--- Assets/Scripts/AI Revision 2/PhysicsAffectedAI.cs
+++ Assets/Scripts/AI Revision 2/PhysicsAffectedAI.cs
@@ -57,14 +57,9 @@ public class PhysicsAffectedAI : MonoBehaviour
         }
         else
         {
-            Transform aiTransform = rootAI.transform;
-            Transform ragdollTransform = ragdoll.transform;
-            Transform ragdollRootBone = ragdoll.rootBone;
-
             // Secretly update the real AI's orientation to match the ragdoll's (while preserving the AI's up axis), then re-parent them.
             // This should hide that the AI and ragdoll were ever separated.
-            aiTransform.position = ragdollTransform.position;
-            aiTransform.rotation = Quaternion.LookRotation(ragdollTransform.forward, aiTransform.up);
+            UpdateBasePositionToMatchRagdoll();
 
             // Transfer the ragdoll velocity back to the base rigidbody
             rigidbody.velocity = ragdoll.totalVelocity;
@@ -73,18 +68,24 @@ public class PhysicsAffectedAI : MonoBehaviour
             ragdoll.totalAngularVelocity = Vector3.zero;
 
             // Reset the ragdoll's transform to zero, but preserve the position of the base bone relative to the AI itself (for animation purposes)
+            Transform ragdollTransform = ragdoll.transform;
+            Transform ragdollRootBone = ragdoll.rootBone;
+
+            // Preserve current position of root bone
             Vector3 rootBoneWorldPosition = ragdollRootBone.position;
             Quaternion rootBoneWorldRotation = ragdollRootBone.rotation;
-            ragdollTransform.position = aiTransform.position;
-            ragdollTransform.localRotation = aiTransform.rotation;
+
+            // Re-assign ragdoll parent and reset its transform orientation
+            ragdollTransform.SetParent(transform);
+            ragdollTransform.localPosition = Vector3.zero;
+            ragdollTransform.localRotation = Quaternion.identity;
+            ragdollTransform.localScale = Vector3.one;
+
+            // Re-assign root bone orientation (so the ragdoll position doesn't visually change)
             ragdollRootBone.position = rootBoneWorldPosition;
             ragdollRootBone.rotation = rootBoneWorldRotation;
-
-            // Re-assign parent
-            ragdollTransform.SetParent(transform);
         }
     }
-
     private void OnEnable()
     {
         // Ensures the various functional elements are active
@@ -96,8 +97,12 @@ public class PhysicsAffectedAI : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        // Don't bother updating anything if the agent is ragdolled
-        if (ragdollActive) return;
+        // If the ragdoll is active, ensure the AI's orientation lines up with the ragdoll's (they're only separated so the physics don't bug out)
+        if (ragdollActive)
+        {
+            UpdateBasePositionToMatchRagdoll();
+            return;
+        }
 
         // Update physics collider size to match NavMeshAgent size
         collider.radius = navMeshAgent.radius;
@@ -111,5 +116,37 @@ public class PhysicsAffectedAI : MonoBehaviour
         {
             rigidbody.MovePosition(currentPosition + (Time.fixedDeltaTime * velocity));
         }
+    }
+
+    public float ragdollUprightDotProduct => Vector3.Dot(ragdoll.rootBone.forward, rootAI.transform.up);
+
+    void UpdateBasePositionToMatchRagdoll()
+    {
+        Transform aiTransform = rootAI.transform;
+        Transform ragdollRootBone = ragdoll.rootBone;
+
+        Vector3 up = aiTransform.up;//-Physics.gravity;
+        
+        // Check what direction the enemy would face after standing up, based on their ragdoll direction
+        // If we compare the direction to up and it's zero, that means it's perfectly forward.
+        // More than 0 means ragdoll is closer to lying on its back. Use the 'down' vector
+        // Less than 0 means ragdoll is closer to lying on its face. Use the 'up' vector
+        Vector3 ragdollForward = ragdollRootBone.forward;
+        float dot = ragdollUprightDotProduct;
+        if (dot < 0)
+        {
+            ragdollForward = ragdollRootBone.up;
+        }
+        else if (dot > 0)
+        {
+            ragdollForward = -ragdollRootBone.up;
+        }
+
+        // Calculate the direction the AI should face in to roughly match the ragdoll (while remaining upright)
+        Vector3 aiForward = Vector3.ProjectOnPlane(ragdollForward, up);
+        // Adjust transform to be roughly in the centre of the ragdoll's body
+        aiTransform.position = rootAI.bounds.center;
+        // Adjust direction to face roughly in the same 'forward' direction as the ragdoll
+        aiTransform.rotation = Quaternion.LookRotation(aiForward, up);
     }
 }
