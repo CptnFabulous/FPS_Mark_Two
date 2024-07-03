296800b726b2f7f4df1374a04c883d74c8488af8
Marcus Mouyat <marcus@mouyat.com>
2024-07-03 23:21:39 +1000

    Refactored StateController to have hierarchical functionality.
    StateControllers now act as their own states and can be switched to from parent StateControllers, resulting in an easy hierarchy system. If a state is switched to, all parent states will switch accordingly to ensure that the desired state is accessed. This allows easy compartmentalisation.
    The new StateController now also knows its immediate children, and the root of the hierarchy.
    I also added a UnityEvent that triggers when a StateController is enabled or disabled, so other scripts can be enabled or disabled along with it.

--- Assets/Scripts/AI Revision 2/StateController.cs
+++ Assets/Scripts/AI Revision 2/StateController.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class StateController : MonoBehaviour
+public class StateController : StateFunction
 {
     [SerializeField] StateFunction current;
+
+    public UnityEngine.Events.UnityEvent<bool> onSetActive;
+
 
     public StateFunction currentState
     {
@@ -12,37 +16,77 @@ public class StateController : MonoBehaviour
         set => SwitchToState(value);
     }
     public StateFunction previousState { get; private set; }
+    public StateFunction[] states { get; private set; }
 
-    public void SwitchToState(StateFunction newState)
+    public override void SwitchToState(StateFunction newState)
     {
-        // Do nothing if already in the current state
-        if (newState == currentState) return;
-
-        // Do nothing if state is not part of the state machine
-        if (newState != null && newState.transform.IsChildOf(transform) == false)
+        // Do nothing if the desired state is not part of the state machine hierarchy
+        if (newState.root != root)
         {
             Debug.LogError($"{this}: cannot switch to {newState} because it's not part of this state machine!");
             return;
         }
 
-        current.enabled = false;
-        previousState = current;
-        current = newState;
-        current.enabled = true;
+        // If not a child of this controller, but still in the hierarchy, initiate from its parent controller instead
+        if (newState.controller != this)
+        {
+            newState.controller.SwitchToState(newState);
+            return;
+        }
+
+        Debug.Log($"{this}: switching from {current} to {newState}");
+        if (newState != currentState)
+        {
+            current.enabled = false;
+            previousState = current;
+            current = newState;
+            current.enabled = true;
+        }
+
+        // Trigger state switch in parent controller
+        // (iterates up to ensure that all necessary parent controllers are active)
+        if (controller != null) controller.SwitchToState(this);
     }
+
     private void Awake()
     {
-        foreach (StateFunction f in GetComponentsInChildren<StateFunction>()) f.enabled = false;
+        // Get all child states (only immediate children)
+        states = GetComponentsInChildren<StateFunction>();
+        states = states.Where(s => s.controller == this).ToArray();
+        // Pre-emptively disables all states
+        foreach (StateFunction f in states) f.enabled = false;
     }
-    private void OnEnable() => currentState.enabled = true;
-    private void OnDisable() => currentState.enabled = false;
+    private void OnEnable() => SetActivity(true);
+    private void OnDisable() => SetActivity(false);
+
+    void SetActivity(bool active)
+    {
+        /*
+        foreach (Behaviour c in subComponents)
+        {
+            c.enabled = active;
+        }
+        */
+        currentState.enabled = active;
+        //onSetActive.Invoke(active);
+    }
 }
 
 public abstract class StateFunction : MonoBehaviour
 {
     StateController _base;
 
-    public StateController controller => _base ??= GetComponentInParent<StateController>();
+    /// <summary>
+    /// The controller that this state is managed by. Only registers if it's in a parent GameObject.
+    /// </summary>
+    public StateController controller => _base ??= transform.parent.GetComponentInParent<StateController>();
+    /// <summary>
+    /// The root controller in a hierarchy.
+    /// <para></para>
+    /// </summary>
+    public StateFunction root => controller == null ? this : controller.root;
+    // If a parent controller is present, ask it for its root.
+    // Once a state can't find a parent controller, that state is the root.
 
-    protected void SwitchToState(StateFunction newState) => controller.SwitchToState(newState);
+    public virtual void SwitchToState(StateFunction newState) => controller.SwitchToState(newState);
 }
