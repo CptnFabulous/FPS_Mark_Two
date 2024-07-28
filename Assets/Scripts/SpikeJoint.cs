7ae93a5024912ab1d42abdb79dfecc4bc67b6668
Marcus Mouyat <marcus@mouyat.com>
2024-07-28 16:32:57 +1000

    Removed unused references from SpikeJoint, that prevented builds from compiling

--- Assets/Scripts/SpikeJoint.cs
+++ Assets/Scripts/SpikeJoint.cs
@@ -3,8 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.UI;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class SpikeJoint : MonoBehaviour
 {
