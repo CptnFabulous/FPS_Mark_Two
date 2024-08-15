e2676df839bf27f78cfb7604433e3ff786026992
Marcus Mouyat <marcus@mouyat.com>
2024-08-15 18:04:16 +1000

    Shootable steam valves (and DamageOnInteract) script
    These valves can be damaged (or just interacted with) to release a burst of steam, similar to the shootable fire extinguishers.
    I created a new script that lets me set up context-sensitive interactions to damage things. This allows the player to quietly activate the steam valve by turning it, instead of attacking it.

---
+++ Assets/Scripts/Entity functions/DamageOnInteract.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageOnInteract : MonoBehaviour
+{
+    public Interactable interactable;
+    public Health health;
+
+    [Header("Damage stats")]
+    public DamageType damageType = DamageType.Piercing;
+    public int damage = 1;
+    public int stun = 1;
+
+    private void Awake()
+    {
+        interactable.canInteract += (_) => health.IsAlive;
+        interactable.onInteract.AddListener(DamageObject);
+    }
+
+    void DamageObject(Player player)
+    {
+        Vector3 direction = interactable.collider.bounds.center - player.bounds.center;
+        health.Damage(damage, stun, false, damageType, player, direction);
+    }
+}
