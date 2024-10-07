83374eaadfce8ea3f60446e73e70a4f752487e85
Marcus Mouyat <marcus@mouyat.com>
2024-10-07 14:13:40 +1100

    Fixed bug where interaction handler would sometimes ignore objects the player is directly looking at
    I fixed this by adding a preliminary raycast straight forward, to check for an interactable object. If this doesn't return anything, then it goes to the 'scan objects within a distance and check for line of sight' code.
    This ensures the player can always reliably select an object by directly aiming at it, regardless of what other stuff is positioned around it.

--- Assets/Scripts/Player/InteractionHandler.cs
+++ Assets/Scripts/Player/InteractionHandler.cs
@@ -36,68 +36,78 @@ public class InteractionHandler : MonoBehaviour
     }
     private void Update()
     {
-        // Reset values
-        targetedInteractable = null;
-        targetedPhysicsProp = null;
-        hitData = new RaycastHit();
+        // Reset values and set up values to assign
         collidersChecked.Clear();
+        Interactable i = null;
+        Rigidbody rb = null;
+        RaycastHit rh = new RaycastHit();
 
-        #region Obtain and sort colliders
-
-        // Obtain initial colliders
-        collidersChecked.AddRange(Physics.OverlapSphere(aimOrigin, interactionRange, detectionMask));
-        // Sort the list first, by which is closest to the centre of the camera
-        // This way we don't have to check the whole list, only until we find one valid option
-        Vector2 aimScreenPoint = referenceCamera.WorldToScreenPoint(aimOrigin + aimDirection);
-        MiscFunctions.SortListWithOnePredicate(collidersChecked, (c) =>
+        // Perform an initial cast, prioritising whatever the player is directly aiming at
+        bool directCast = InteractionCast(aimDirection, out rh) && ColliderIsInteractable(rh.collider, out i, out rb);
+        // If that didn't return anything, do a sweep for other objects within the desired range and angle
+        if (directCast == false)
         {
-            // Get screen position of bounds centre, and compare it to the position of the player's reticle
-            // (use sqrMagnitude since we don't need the exact distance)
-            Vector2 screenPosition = referenceCamera.WorldToScreenPoint(c.bounds.center);
-            return (screenPosition - aimScreenPoint).sqrMagnitude;
-        });
-
-        #endregion
-
-        // Get first entry that meets the criteria
-        foreach (Collider c in collidersChecked)
-        {
-            // Check for either an Interactable or Rigidbody, ignore if not found
-            Interactable i = c.GetComponentInParent<Interactable>();
-            Rigidbody rb = null;
-            if (objectCarrier != null)
-            {
-                // Check for a root rigidbody, and then check if the player can pick it up
-                rb = c.GetComponentInParent<Rigidbody>();
-                rb = PhysicsCache.GetRootRigidbody(rb);
-                if (objectCarrier.CanPickUpObject(rb) == false) rb = null;
-            }
-            if (i == null && rb == null) continue;
-
-            // Check if within field of view, ignore if not
-            if (Vector3.Angle(aimDirection, c.bounds.center - aimOrigin) > detectionAngle) continue;
-
-            // Check for line of sight, ignore if the raycast didn't hit the desired collider, or at all
-            Ray ray = new Ray(aimOrigin, c.bounds.center - aimOrigin);
-            if (Physics.Raycast(ray, out RaycastHit rh, interactionRange, detectionMask) == false) continue;
-            if (rh.collider != c) continue;
-
-            // We've found the closest interactable! Mark it as the highlighted one
-            targetedInteractable = i;
-            targetedPhysicsProp = rb;
-            hitData = rh;
-            // Get bounds
-            if (i != null)
-            {
-                targetBounds = i.collider.bounds;
-            }
-            else if (rb != null)
-            {
-                targetBounds = MiscFunctions.CombinedBounds(PhysicsCache.GetChildColliders(rb));
-            }
             
-            break;
+            // Obtain initial colliders
+            collidersChecked.AddRange(Physics.OverlapSphere(aimOrigin, interactionRange, detectionMask));
+            // Sort the list first, by which is closest to the centre of the camera
+            // This way we don't have to check the whole list, only until we find one valid option
+            Vector2 aimScreenPoint = referenceCamera.WorldToScreenPoint(aimOrigin + aimDirection);
+            MiscFunctions.SortListWithOnePredicate(collidersChecked, (c) =>
+            {
+                // Get screen position of bounds centre, and compare it to the position of the player's reticle
+                // (use sqrMagnitude since we don't need the exact distance)
+                Vector2 screenPosition = referenceCamera.WorldToScreenPoint(c.bounds.center);
+                return (screenPosition - aimScreenPoint).sqrMagnitude;
+            });
+
+            // Get first entry that meets the criteria
+            foreach (Collider c in collidersChecked)
+            {
+                // Check for either an Interactable or Rigidbody, ignore if not found
+                if (ColliderIsInteractable(c, out i, out rb) == false) continue;
+
+                // Check if this collider is within line of sight and view angle (ignore if not)
+                Vector3 directionToTarget = c.bounds.center - aimOrigin;
+                if (Vector3.Angle(aimDirection, directionToTarget) > detectionAngle) continue;
+                if (InteractionCast(directionToTarget, out rh) == false) continue;
+                if (rh.collider != c) continue;
+
+                // Cancel loop, we've found an interactable!
+                break;
+            }
         }
+
+        // Cache the data on whatever we hit this frame
+        targetedInteractable = i;
+        targetedPhysicsProp = rb;
+        hitData = rh;
+        // Get bounds
+        if (i != null)
+        {
+            targetBounds = i.collider.bounds;
+        }
+        else if (rb != null)
+        {
+            targetBounds = MiscFunctions.CombinedBounds(PhysicsCache.GetChildColliders(rb));
+        }
+    }
+
+    bool InteractionCast(Vector3 direction, out RaycastHit rh) => Physics.Raycast(aimOrigin, direction, out rh, interactionRange, detectionMask);
+    bool ColliderIsInteractable(Collider c, out Interactable i, out Rigidbody rb)
+    {
+        // Check for either an Interactable or Rigidbody, ignore if not found
+        i = c.GetComponentInParent<Interactable>();
+        rb = null;
+        if (objectCarrier != null)
+        {
+            // Check for a root rigidbody, and then check if the player can pick it up
+            rb = c.GetComponentInParent<Rigidbody>();
+            rb = PhysicsCache.GetRootRigidbody(rb);
+            if (objectCarrier.CanPickUpObject(rb) == false) rb = null;
+        }
+
+        return i != null || rb != null;
     }
 
     protected virtual void AttemptInteraction()
