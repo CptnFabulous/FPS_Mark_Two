d8aa904d6ab2b644d4b3011a433854881ff9d2ac
Marcus Mouyat <marcus@mouyat.com>
2024-10-22 13:18:29 +1100

    New objective to complete a level without dying
    This works with the CheckpointManager to show how many times a player has died throughout the level, and reward them if they avoid doing so.
    This objective will be removed once I add an actual save/load system, as later on the player will actually die and have to start from their last save.

---
+++ Assets/Scripts/Player progression/AvoidDyingObjective.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvoidDyingObjective : Objective
+{
+    public CheckpointManager checkpointManager;
+    public string deathMessageFormatting = "Deaths: {0}";
+
+    int deaths => checkpointManager.deathCount;
+
+    public override Vector3 location => Vector3.zero;
+
+
+    public override string formattedProgress
+    {
+        get
+        {
+            if (deaths == 0) return base.formattedProgress;
+
+            return string.Format(deathMessageFormatting, deaths);
+        }
+    }
+
+    protected override bool DetermineFailure() => deaths > 0;
+    protected override bool DetermineSuccess() => deaths <= 0;
+
+    protected override string GetSerializedProgress()
+    {
+        return "";
+        //throw new System.NotImplementedException();
+    }
+
+    protected override void Setup(string progress)
+    {
+        //throw new System.NotImplementedException();
+    }
+}
