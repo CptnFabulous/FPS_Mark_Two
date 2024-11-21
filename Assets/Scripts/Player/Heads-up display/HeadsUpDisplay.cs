94c43a112fa157ffa70346b4d22d07989d3ccfea
Marcus Mouyat <marcus@mouyat.com>
2024-11-21 19:53:30 +1100

    WIP: hit effects now play if an enemy was hurt by physics damage, adding more intrinsic reward for indirect kills

--- Assets/Scripts/Player/Heads-up display/HeadsUpDisplay.cs
+++ Assets/Scripts/Player/Heads-up display/HeadsUpDisplay.cs
@@ -74,10 +74,17 @@ public class HeadsUpDisplay : MonoBehaviour
     public UnityEvent killEffects;
     public void CheckToPlayDamageEffects(DamageMessage message)
     {
-        if (message.attacker != controller)
-        {
-            return;
-        }
+        // Don't play effects for self-damage or self-healing
+        if (message.victim == controller.health) return;
+
+        if (message.method == DamageType.Healing) return;
+        if (message.method == DamageType.DeletionByGame) return;
+
+        // Don't play effects if another enemy was directly dealing the damage
+        // I need to update this later to detect if an indirect form of damage was still caused by the player (so it can appropriately reward the player for actually setting up said kills)
+        Entity attacker = message.attacker;
+        if (attacker is Character c && c != controller && message.method != DamageType.PhysicsImpact) return;
+        //if (attacker != controller) return;
 
         damageEffects.Invoke();
     }
