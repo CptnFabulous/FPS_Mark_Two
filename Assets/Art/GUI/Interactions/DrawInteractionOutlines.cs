5aee6214ad52fc169e5176e5c2070b9648ec84c3
Marcus Mouyat <marcus@mouyat.com>
2024-12-30 14:06:17 +1100

    WIP: Interaction outline
    I created a custom render feature that reads the player’s interaction handler, and draws an inverse hull outline around it. The system displays differently coloured outlines for if an interactable is enabled or disabled, or if it’s just a physics prop.
    This system works fairly well, but the outline itself doesn’t look great. The shape can be wonky and inconsistent, and parts of the outline will still clip in front of the object. That’s all minor cosmetic stuff though, that I can fix later.

---
+++ Assets/Art/GUI/Interactions/DrawInteractionOutlines.cs
@@ -0,0 +1,98 @@
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class DrawInteractionOutlines : ScriptableRendererFeature
+{
+    public Material interactableHighlight;
+    public Material nonInteractableHighlight;
+    public Material physicsPropHighlight;
+    public RenderPassEvent renderPassEvent = RenderPassEvent.AfterRenderingOpaques;
+
+    class CustomRenderPass : ScriptableRenderPass
+    {
+        public Material interactableHighlight;
+        public Material nonInteractableHighlight;
+        public Material physicsPropHighlight;
+        public InteractionHandler interactionHandler;
+        
+        // Here you can implement the rendering logic.
+        // Use <c>ScriptableRenderContext</c> to issue drawing commands or execute command buffers
+        // https://docs.unity3d.com/ScriptReference/Rendering.ScriptableRenderContext.html
+        // You don't have to call ScriptableRenderContext.submit, the render pipeline will call it at specific points in the pipeline.
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            // Check if this camera is on a player with an active interaction handler
+            if (interactionHandler == null) return;
+            if (interactionHandler.enabled == false) return;
+            
+            // Check that the player is looking at something interactable
+            Interactable i = interactionHandler.targetedInteractable;
+            Rigidbody rb = interactionHandler.targetedPhysicsProp;
+            if (i == null && rb == null) return;
+
+
+            CommandBuffer cmd = CommandBufferPool.Get("Interaction Outline Pass");
+            context.ExecuteCommandBuffer(cmd);
+            cmd.Clear();
+
+            // Determine which highlight to use based on if it's a context-sensitive interaction or a physics object
+
+            Material m;
+            if (i != null)
+            {
+                bool canInteract = interactionHandler.canInteractWithTarget;
+                m = canInteract ? interactableHighlight : nonInteractableHighlight;
+            }
+            else
+            {
+                m = physicsPropHighlight;
+            }
+
+            //bool isInteractable = i != null;
+            //Material m = isInteractable ? interactableHighlight : physicsPropHighlight;
+
+            RaycastHit rh = interactionHandler.hitData;
+            LayerMask cameraMask = renderingData.cameraData.camera.cullingMask;
+            foreach (Renderer r in ComponentCache<Entity>.Get(rh.collider.gameObject).renderers)
+            {
+                if (MiscFunctions.IsLayerInLayerMask(cameraMask, r.gameObject.layer) == false) return;
+                cmd.DrawRenderer(r, m);
+            }
+
+            context.ExecuteCommandBuffer(cmd);
+            CommandBufferPool.Release(cmd);
+        }
+    }
+
+    CustomRenderPass m_ScriptablePass;
+
+    /// <inheritdoc/>
+    public override void Create()
+    {
+        m_ScriptablePass = new CustomRenderPass();
+        m_ScriptablePass.interactableHighlight = interactableHighlight;
+        m_ScriptablePass.nonInteractableHighlight = nonInteractableHighlight;
+        m_ScriptablePass.physicsPropHighlight = physicsPropHighlight;
+        // Configures where the render pass should be injected.
+        m_ScriptablePass.renderPassEvent = renderPassEvent;
+    }
+
+    // Here you can inject one or multiple render passes in the renderer.
+    // This method is called when setting up the renderer once per-camera.
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        // Get interaction handler
+        Camera c = renderingData.cameraData.camera;
+        Player p = c.GetComponentInParent<Player>();
+        if (p == null) return;
+        InteractionHandler ih = p.GetComponentInChildren<InteractionHandler>();
+        if (ih == null) return;
+
+        m_ScriptablePass.interactionHandler = ih;
+        renderer.EnqueuePass(m_ScriptablePass);
+    }
+}
+
+
