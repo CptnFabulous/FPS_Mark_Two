3a2ef854c9b8987ddfacd00a5dfa162be7d182c8
Marcus Mouyat <marcus@mouyat.com>
2025-02-01 20:52:22 +1100

    Removed unrecognised namespaces that were causing compiler errors

--- Assets/Scripts/Player/SprintController.cs
+++ Assets/Scripts/Player/SprintController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Events;
-using Newtonsoft.Json.Linq;
 
 public class SprintController : MonoBehaviour
 {
