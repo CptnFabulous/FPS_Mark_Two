4f84ddb57b1714e6c4b1ff9dffd215262c770654
Marcus Mouyat <marcus@mouyat.com>
2025-03-03 13:11:47 +1100

    Squashed commit of the following:
    commit 49e1d5e737e7faffa6e5062bf5d4c396a3939517
    Author: Marcus Mouyat <marcus@mouyat.com>
    Date:   Mon Mar 3 13:10:12 2025 +1100
    
        Re-implemented smoke grenade and steam valve improvements
    
        Turns out it was the fire extinguisher changes that were responsible for the 'enemies shooting through walls' bug, so I'll need to fix the fire extinguisher changes later.
    
    commit 19535696643deef2ba4698da05f08666c72b8209
    Author: Marcus Mouyat <marcus@mouyat.com>
    Date:   Sun Feb 23 15:31:58 2025 +1100
    
        Revert "Improvements to smoke clouds"
    
        I reverted commit 84809805daba2f8e9d2494a4456bab7a16dab9df, as it somehow caused a gamebreaking bug where enemies could shoot you through walls. I have no idea what causes this, but I know that reverting this commit was able to fix it.

--- Assets/Scripts/Player Weapons/SmokeGrenade.cs
+++ Assets/Scripts/Player Weapons/SmokeGrenade.cs
@@ -5,11 +5,32 @@ using UnityEngine;
 public class SmokeGrenade : Throwable
 {
     public SmokeCloud smoke;
+    public float remainingDuration = 10;
 
     public override void OnThrow() { }
 
     private void OnEnable()
     {
+        //Debug.Log($"{this}: enabled at time {Time.time}");
         smoke.emitting = true;
     }
+    private void OnDisable()
+    {
+        //Debug.Log($"{this}: disabled at time {Time.time}");
+        //smoke.activelyEmitting = false;
+    }
+    /*
+    void Update()
+    {
+        if (smoke.emitting == false)
+        {
+            enabled = false;
+            return;
+        }
+
+        remainingDuration = Mathf.Max(remainingDuration - Time.deltaTime, 0);
+        //Debug.Log("Remaining duration = " + remainingDuration);
+        smoke.emitting = remainingDuration > 0;
+    }
+    */
 }
