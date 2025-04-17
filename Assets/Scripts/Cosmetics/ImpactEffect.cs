266f68fa789fb182aa3bb68443e4ff51923b4cd4
Marcus Mouyat <marcus@mouyat.com>
2025-04-17 18:10:44 +1000

    Object pooling code is now located in a standalone package
    I took the object pooling code and placed it in its own package. This way, I can re-use it in other projects, and its standalone nature looks much better on a portfolio.

--- Assets/Scripts/Cosmetics/ImpactEffect.cs
+++ Assets/Scripts/Cosmetics/ImpactEffect.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CptnFabulous.ObjectPool;
 
 [CreateAssetMenu(fileName = "New Impact Effect", menuName = "ScriptableObjects/Impact Effect", order = 0)]
 public class ImpactEffect : ScriptableObject
--- Assets/Scripts/Cosmetics/WorldSpaceIconDrawer.cs
+++ Assets/Scripts/Cosmetics/WorldSpaceIconDrawer.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
+using CptnFabulous.ObjectPool;
 
 public static class WorldSpaceIconDrawer
 {
