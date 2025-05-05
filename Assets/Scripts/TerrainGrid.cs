85c80a367a8b8a447b413e986e250b80790e21ea
Marcus Mouyat <marcus@mouyat.com>
2025-05-05 13:09:13 +1000

    TerrainGrid tweaks - margin on bounds, grid now has full Bounds struct, and bool confirming that everything's been generated

--- Assets/Scripts/TerrainGrid.cs
+++ Assets/Scripts/TerrainGrid.cs
@@ -25,19 +25,22 @@ public class TerrainGrid : MonoBehaviour
 
     #endregion
 
-    public int resolutionScale = 1;
     public LayerMask terrainDetection;
+    public Vector3 boundsMargins = Vector3.one;
+    public int resolutionScale = 1;
     public Texture3D debugTerrainTexture;
 
-    Vector3Int _gridBoundsMin;
-    Vector3Int _gridBoundsMax;
+    bool generated = false;
+    Bounds _bounds;
+    BoundsInt _gridBounds;
     bool[,,] _occupiedByTerrain;
     Vector3Int _textureResolution;
     
     public Mesh mapMesh { get; private set; }
-    public Bounds bounds { get; private set; }
-    public Vector3Int gridBoundsMin => _gridBoundsMin;
-    public Vector3Int gridBoundsMax => _gridBoundsMax;
+    public Bounds bounds => _bounds;
+    public BoundsInt gridBounds => _gridBounds;
+    public Vector3Int gridBoundsMin => gridBounds.min;
+    public Vector3Int gridBoundsMax => gridBounds.max;
     public bool[,,] containsTerrain => _occupiedByTerrain;
     public Vector3Int textureResolution => _textureResolution;
 
@@ -47,9 +50,17 @@ public class TerrainGrid : MonoBehaviour
     {
         GenerateMap();
     }
+    private void OnDrawGizmosSelected()
+    {
+        if (generated == false) return;
+
+        Gizmos.DrawWireCube(bounds.center, bounds.size);
+    }
 
     void GenerateMap()
     {
+        if (generated == false) return;
+
         #region Create mesh from NavMesh data
 
         NavMeshTriangulation triangulation = NavMesh.CalculateTriangulation();
@@ -64,13 +75,24 @@ public class TerrainGrid : MonoBehaviour
 
         #region Create grid bounds
 
-        bounds = mapMesh.bounds;
+        _bounds = mapMesh.bounds;
+        // Expand bounds by margins, to ensure nothing is left out
+        _bounds.extents += boundsMargins;
+
+        Vector3Int min = new Vector3Int();
+        Vector3Int max = new Vector3Int();
+        for (int i = 0; i < 3; i++)
+        {
+            min[i] = Mathf.FloorToInt(bounds.min[i]);
+            max[i] = Mathf.CeilToInt(bounds.max[i]);
+        }
+        _gridBounds.min = min;
+        _gridBounds.max = max;
+
         _textureResolution = Vector3Int.zero;
         for (int i = 0; i < 3; i++)
         {
-            _gridBoundsMin[i] = Mathf.FloorToInt(bounds.min[i]);
-            _gridBoundsMax[i] = Mathf.CeilToInt(bounds.max[i]);
-            _textureResolution[i] = gridBoundsMax[i] - gridBoundsMin[i];
+            _textureResolution[i] = max[i] - min[i];
             _textureResolution[i] *= resolutionScale;
         }
 
@@ -78,23 +100,19 @@ public class TerrainGrid : MonoBehaviour
 
         #region Create 3D texture from geometry
 
-        debugTerrainTexture = new Texture3D(textureResolution.x, textureResolution.y, textureResolution.z, TextureFormat.ARGB32, false);
-        _occupiedByTerrain = new bool[textureResolution.x, textureResolution.y, textureResolution.z];
-
         Vector3 checkBoxHalfExtents = 0.5f / resolutionScale * Vector3.one;
 
-        Vector3Int min = Vector3Int.zero;
-        Vector3Int max = textureResolution;
-        for (int x = min.x; x < max.x; x++)
+        _occupiedByTerrain = new bool[textureResolution.x, textureResolution.y, textureResolution.z];
+        debugTerrainTexture = new Texture3D(textureResolution.x, textureResolution.y, textureResolution.z, TextureFormat.ARGB32, false);
+        for (int x = 0; x < textureResolution.x; x++)
         {
-            for (int y = min.y; y < max.y; y++)
+            for (int y = 0; y < textureResolution.y; y++)
             {
-                for (int z = min.z; z < max.z; z++)
+                for (int z = 0; z < textureResolution.z; z++)
                 {
                     Vector3 worldPos = WorldPositionFromTextureCoordinates(new Vector3Int(x, y, z));
                     bool containsTerrain = Physics.CheckBox(worldPos, checkBoxHalfExtents, Quaternion.identity, terrainDetection);
                     _occupiedByTerrain[x, y, z] = containsTerrain;
-
                     debugTerrainTexture.SetPixel(x, y, z, containsTerrain ? Color.white : Color.clear);
                 }
             }
@@ -104,6 +122,8 @@ public class TerrainGrid : MonoBehaviour
         #endregion
 
         doorsInScene = FindObjectsOfType<Door>();
+
+        generated = true;
     }
     public Vector3Int TextureCoordinatesFromWorldPosition(Vector3 position)
     {
