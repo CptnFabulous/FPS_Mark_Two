75fb57cdfd8be5415d496e45d2f49d113cde161d
Marcus Mouyat <marcus@mouyat.com>
2025-08-25 17:53:17 +1000

    WIP on new offhand attack - fire extinguisher
    In addition to the larger fire extinguishers that can be picked up and thrown, this smaller one can be held and sprayed right in an enemy's face! Compared to the smoke grenades, it allows more convenient cloud creation, with the downside of reduced capacity and duration.
    
    This is still very much a work in progress and there are bugs to iron out, such as the cloud not appearing properly the first time it's fired. There are also balance tweaks to deal with. But it's proving very promising so far!

---
+++ Assets/Scripts/Player Weapons/RangedAttackSmokeEmitter.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CptnFabulous.ObjectPool;
+using UnityEngine.InputSystem.HID;
+
+public class RangedAttackSmokeEmitter : MonoBehaviour
+{
+    public ContinuousRangedAttackData attachedTo;
+    public SmokeCloud smokeCloudPrefab;
+
+    SmokeCloud assignedSmokeCloud;
+
+    private void Awake()
+    {
+        if (smokeCloudPrefab != null)
+        {
+            attachedTo.onEnable.AddListener(RequestParticleSystem);
+            attachedTo.onDisable.AddListener(DismissParticleSystem);
+        }
+    }
+
+    void RequestParticleSystem()
+    {
+        if (assignedSmokeCloud != null) return;
+
+        // Create an object pool and request a particle system from it.
+        // Determine settings beforehand, to make sure the particle system isn't disabled after being dismissed.
+        // This ensures the spawned particles continue to exist in the world even when the weapon is holstered.
+        ObjectPool.CreateObjectPool(smokeCloudPrefab, true, 0, false);
+        assignedSmokeCloud = ObjectPool.RequestObject(smokeCloudPrefab);
+
+        // Make it a child of the muzzle
+        Transform psTransform = assignedSmokeCloud.transform;
+        psTransform.parent = attachedTo.muzzle;
+        psTransform.localPosition = Vector3.zero;
+        psTransform.localRotation = Quaternion.identity;
+        psTransform.localScale = Vector3.one;
+
+        assignedSmokeCloud.emitting = true;
+    }
+    void DismissParticleSystem()
+    {
+        if (assignedSmokeCloud == null) return;
+
+        // Turn off particle system and dismiss it to the object pool.
+        assignedSmokeCloud.emitting = false;
+        ObjectPool.DismissObject(assignedSmokeCloud);
+        // Clear reference
+        assignedSmokeCloud = null;
+    }
+}
