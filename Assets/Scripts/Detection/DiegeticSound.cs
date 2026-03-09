1eea4fc95b7b42e64c7d45652ff0968a419563bc
Marcus Mouyat <marcus@mouyat.com>
2026-03-09 12:12:42 +1100

    AI sound check optimisation
    Every single time a sound played, it would perform an AI check. This proved very redundant and very laggy if multiples of the same sound played very close to each other at the same time, e.g. multiple physics objects colliding with each other. I fixed this by adding a cooldown time and minimum distance. Each time an AI check plays for a particular sound, it records the time and location to a struct in a HashSet. Each time a sound plays, it checks all AI checks performed recently (removing ones too old to worry about), and if a recent check was played too close to the current sound instance, it won’t do another check. I also cleaned up
    This change has resulted in massive performance improvements, and I’m really pleased with how little tweaking I had to do to get it right.

--- Assets/Scripts/Detection/DiegeticSound.cs
+++ Assets/Scripts/Detection/DiegeticSound.cs
@@ -1,13 +1,25 @@
-﻿//using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.Drawing;
 using UnityEngine;
-using UnityEngine.Audio;
 
 [CreateAssetMenu(fileName = "New Diegetic Sound", menuName = "ScriptableObjects/Diegetic Sound", order = 1)]
 public class DiegeticSound : ScriptableObject
 {
+    /// <summary>
+    /// A private struct used just to check how recently a sound played in the same location.
+    /// </summary>
+    struct PlayedSound
+    {
+        public Vector3 checkOriginPoint;
+        public float time;
+
+        public PlayedSound(Vector3 point, float time)
+        {
+            this.checkOriginPoint = point;
+            this.time = time;
+        }
+    }
+
     [SerializeField] AudioClip[] sounds;
     //[SerializeField] AudioMixerGroup mixerGroup;
     [SerializeField] float decibels = 80;
@@ -18,6 +30,10 @@ public class DiegeticSound : ScriptableObject
     [SerializeField, Range(0, 1)] float maxVolumeVariance = 1;
     [SerializeField] float delay;
     */
+
+    HashSet<PlayedSound> recentSoundChecks = new HashSet<PlayedSound>();
+    static readonly float distanceThreshold = 1f;
+    static readonly float ageThreshold = 0.2f;
 
     public void Play(Entity entity) => Play(entity.bounds.center, entity);
     public void Play(AudioSource source) => Play(source.transform.position, source.GetComponentInParent<Entity>(), source);
@@ -30,7 +46,6 @@ public class DiegeticSound : ScriptableObject
         }
         Play(point, sourceEntity, source, multiplier, playAudioAtMaxVolume);
     }
-
     public void Play(Vector3 point, Entity sourceEntity, AudioSource source, float multiplier = 1, bool playAudioAtMaxVolume = false)
     {
         AudioClip clip = sounds[Random.Range(0, sounds.Length)];
@@ -51,7 +66,29 @@ public class DiegeticSound : ScriptableObject
 
         // Play diegetic code (assuming there's an entity to check against)
         if (sourceEntity == null) return;
-        DiegeticAudioListener.DiegeticCheck(this, decibels * multiplier, sourceEntity);
 
+        #region Check if an AI check was done for this sound recently near this point. If so, don't do an AI check until that one has expired.
+
+        // Remove all sounds played before the age threshold.
+        // If a sound is still in, then that always means it's new enough that other checks shouldn't be run near that position.
+        // This acts as an age check and keeps the hashset length small, at the same time.
+        float oldestAcceptableTime = Time.unscaledTime - ageThreshold;
+        recentSoundChecks.RemoveWhere((ps) => ps.time < oldestAcceptableTime);
+
+        // If a recent instance of this sound was played near this point, don't do an AI hearing check.
+        Vector3 checkOriginPoint = sourceEntity.CentreOfMass;
+        foreach (PlayedSound sound in recentSoundChecks)
+        {
+            float distance = Vector3.Distance(sound.checkOriginPoint, checkOriginPoint);
+            if (distance < distanceThreshold) return;
+        }
+
+        // Ensure this sound check's position is logged in the hashset.
+        recentSoundChecks.Add(new PlayedSound(checkOriginPoint, Time.unscaledTime));
+
+        #endregion
+
+        // Do the actual check
+        DiegeticAudioListener.DiegeticCheck(this, decibels * multiplier, sourceEntity);
     }
 }
