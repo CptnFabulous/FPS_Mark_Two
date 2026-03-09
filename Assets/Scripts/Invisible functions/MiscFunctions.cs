21b6d2e4ede5bb090d8c163475d38642488e6ce2
Marcus Mouyat <marcus@mouyat.com>
2026-03-09 12:56:50 +1100

    Moved 'try set animator variable' functions to MiscFunctions (I forgot to include this in a previous commit)

--- Assets/Scripts/Invisible functions/MiscFunctions.cs
+++ Assets/Scripts/Invisible functions/MiscFunctions.cs
@@ -568,6 +568,41 @@ public readonly struct MiscFunctions
     #endregion
 
 
+    #region Animation
+    public static bool TrySetAnimatorTrigger(Animator animator, string name)
+    {
+        if (!AnimatorParameterExists(animator, name)) return false;
+        animator.SetTrigger(name);
+        return true;
+    }
+    public static bool TrySetAnimatorBool(Animator animator, string name, bool value)
+    {
+        if (!AnimatorParameterExists(animator, name)) return false;
+        animator.SetBool(name, value);
+        return true;
+    }
+    public static bool TrySetAnimatorInteger(Animator animator, string name, int value)
+    {
+        if (!AnimatorParameterExists(animator, name)) return false;
+        animator.SetInteger(name, value);
+        return true;
+    }
+    public static bool TrySetAnimatorFloat(Animator animator, string name, float value)
+    {
+        if (!AnimatorParameterExists(animator, name)) return false;
+        animator.SetFloat(name, value);
+        return true;
+    }
+    public static bool AnimatorParameterExists(Animator animator, string name)
+    {
+        foreach (AnimatorControllerParameter parameter in animator.parameters)
+        {
+            if (parameter.name == name) return true;
+        }
+        return false;
+    }
+    #endregion
+
     public static int ClosestAxis(Vector3 direction)
     {
         int bestAxis = 0;
