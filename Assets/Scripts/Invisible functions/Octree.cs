2c66ba396b8f06830dc06d4aed778495a819452d
Marcus Mouyat <marcus@mouyat.com>
2026-07-10 11:18:09 +1000

    Octree WIP: leaf data is stored more effectively
    I have more work to do on this to make it an intuitive and usable system, I'm still learning and figuring things out.

--- Assets/Scripts/Invisible functions/Octree.cs
+++ Assets/Scripts/Invisible functions/Octree.cs
@@ -7,6 +7,7 @@ public class Octant<T>
 {
     public Vector3Int min = Vector3Int.zero;
     public Octant<T>[] children = new Octant<T>[8];
+    public bool isLeaf;
     public T leafData;
 }
 public class Octree<T>
@@ -19,9 +20,14 @@ public class Octree<T>
 
     public Octant<T> baseContainer => _base;
     
-    public void Refresh() => SearchOctant(_base, subdivisions);
+    public void Refresh()
+    {
+        //SearchOctant(_base, subdivisions);
+        RefreshOctant(_base, subdivisions);
+    }
     public void DrawGizmos() => DrawOctantGizmos(_base, subdivisions);
 
+    /*
     void SearchOctant(Octant<T> octant, int sizePower)
     {
         if (octant == null) return;
@@ -68,6 +74,56 @@ public class Octree<T>
             SearchOctant(octant.children[i], sizePower - 1);
         }
     }
+    */
+    void RefreshOctant(Octant<T> octant, int sizePower)
+    {
+        if (octant == null) return;
+
+        octant.isLeaf = false;
+
+        // The size of the entire octant
+        int size = CalculateGridSize(sizePower);
+        Vector3Int dimensions = new Vector3Int(size, size, size);
+        Vector3Int max = octant.min + dimensions;
+        bool possibleChildrenDetected = checkOctant.Invoke(octant.min, max);
+
+
+        // If no more children can be found, record leaf data and return
+        if (!possibleChildrenDetected)
+        {
+            if (recordLeafData != null) octant.leafData = recordLeafData.Invoke(octant.min, max);
+            octant.isLeaf = true;
+            // Clear all child slots
+            for (int i = 0; i < 8; i++) octant.children[i] = null;
+            return;
+        }
+
+        // TO DO: if size at depth is 1 or less, that means we've subdivided as far as we can go.
+        if (size <= 1)
+        {
+            // Do nothing?
+            return;
+        }
+
+        int childSize = size / 2;
+        Vector3Int childDimensions = new Vector3Int(childSize, childSize, childSize);
+
+        for (int i = 0; i < 8; i++)
+        {
+            // Calculate check min and max
+            Vector3Int offset = SmokeParticleDensityController.neighbourOffsets[i];
+            offset.x *= childSize;
+            offset.y *= childSize;
+            offset.z *= childSize;
+            Vector3Int childMin = octant.min + offset;
+            Vector3Int childMax = childMin + childDimensions;
+
+            if (octant.children[i] == null) octant.children[i] = new Octant<T>();
+
+            octant.children[i].min = childMin;
+            RefreshOctant(octant.children[i], sizePower - 1);
+        }
+    }
     void DrawOctantGizmos<T>(Octant<T> octant, int sizePower)
     {
         if (octant == null) return;
@@ -75,19 +131,29 @@ public class Octree<T>
         // Size
         int sizeAlongAxis = CalculateGridSize(sizePower);
         Vector3 size = new Vector3(sizeAlongAxis, sizeAlongAxis, sizeAlongAxis);
-        // Colour
-        float colourLerp = 1 - ((float)sizePower / (float)subdivisions);
-        Gizmos.color = Color.Lerp(Color.white, Color.black, colourLerp);
+        Vector3 centre = octant.min + (size / 2);
 
-        // Draw opaque boxes to indicate smallest sections
-        if (sizeAlongAxis <= 1)
+        if (octant.isLeaf)
         {
-            Gizmos.DrawCube(octant.min + (size / 2), size);
-            return;
+            //Gizmos.color = new Color(0, 0.5f, 0.5f);
+            float colourLerp = 1 - ((float)sizePower / (float)subdivisions);
+            Gizmos.color = Color.Lerp(Color.white, Color.black, colourLerp);
+            //Gizmos.DrawCube(centre, size);
+            Gizmos.DrawWireCube(centre, size);
+            //Gizmos.DrawWireSphere(centre, sizeAlongAxis / 2);
+            //return;
+        }
+        else
+        {
+            // Colour
+            float colourLerp = 1 - ((float)sizePower / (float)subdivisions);
+            Gizmos.color = Color.Lerp(Color.white, Color.black, colourLerp);
+            // Draw
+            Gizmos.DrawWireCube(centre, size);
         }
 
-        // Draw
-        Gizmos.DrawWireCube(octant.min + (size / 2), size);
+        
+
         // Draw gizmos for each child
         for (int i = 0; i < 8; i++)
         {
